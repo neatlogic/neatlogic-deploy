037f5e224275da522913545dcde77c741ee96957
lvzk <lvzk@techsure.com.cn>
2022-05-27 10:04:17 +0800

    update deploy package

--- src/main/java/codedriver/module/deploy/api/DeployAppEnvAutoConfigSaveApi.java
+++ src/main/java/codedriver/module/deploy/api/DeployAppEnvAutoConfigSaveApi.java
@@ -3,7 +3,7 @@
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
-package codedriver.module.deploy.app;
+package codedriver.module.deploy.api;
 
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.deploy.dto.app.DeployAppEnvAutoConfigVo;
