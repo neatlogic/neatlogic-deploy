4150c5352faf793e439376a310722a1585a13baa
linbq@techsure.com.cn <Besiter789>
2022-06-15 18:54:30 +0800

    --task=1006449 --user=林邦泉 后端-应用配置-流水线引用预置参数Handler https://www.tapd.cn/54247054/s/1235312

--- src/main/java/codedriver/module/deploy/dependency/handler/AutoexecGlobalParam2DeployAppPipelinePhaseOperationArgumentParamDependencyHandler.java
+++ src/main/java/codedriver/module/deploy/dependency/handler/AutoexecGlobalParam2DeployAppPipelinePhaseOperationArgumentParamDependencyHandler.java
@@ -6,14 +6,25 @@
 package codedriver.module.deploy.dependency.handler;
 
 import codedriver.framework.autoexec.constvalue.AutoexecFromType;
+import codedriver.framework.autoexec.dto.combop.AutoexecCombopPhaseConfigVo;
+import codedriver.framework.autoexec.dto.combop.AutoexecCombopPhaseOperationConfigVo;
+import codedriver.framework.autoexec.dto.combop.AutoexecCombopPhaseOperationVo;
 import codedriver.framework.dependency.core.FixedTableDependencyHandlerBase;
 import codedriver.framework.dependency.core.IFromType;
 import codedriver.framework.dependency.dto.DependencyInfoVo;
 import codedriver.framework.dependency.dto.DependencyVo;
+import codedriver.framework.deploy.dto.app.DeployAppConfigVo;
+import codedriver.framework.deploy.dto.app.DeployPipelineConfigVo;
+import codedriver.framework.deploy.dto.app.DeployPipelinePhaseVo;
 import codedriver.module.deploy.dao.mapper.DeployAppConfigMapper;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
 
 @Component
 public class AutoexecGlobalParam2DeployAppPipelinePhaseOperationArgumentParamDependencyHandler extends FixedTableDependencyHandlerBase {
@@ -22,12 +33,69 @@ public class AutoexecGlobalParam2DeployAppPipelinePhaseOperationArgumentParamDep
     private DeployAppConfigMapper deployAppConfigMapper;
 
     @Override
-    public IFromType getFromType() {
-        return AutoexecFromType.GLOBAL_PARAM;
+    protected DependencyInfoVo parse(DependencyVo dependencyVo) {
+        JSONObject config = dependencyVo.getConfig();
+        if (MapUtils.isEmpty(config)) {
+            return null;
+        }
+        Long appSystemId = config.getLong("appSystemId");
+        Long moduleId = config.getLong("moduleId");
+        Long envId = config.getLong("envId");
+        DeployAppConfigVo searchVo = new DeployAppConfigVo(appSystemId, moduleId, envId);
+        DeployAppConfigVo deployAppConfigVo = deployAppConfigMapper.getAppConfigVo(searchVo);
+        if (deployAppConfigVo == null) {
+            return null;
+        }
+        DeployPipelineConfigVo pipelineConfigVo = deployAppConfigVo.getConfig();
+        if (pipelineConfigVo == null) {
+            return null;
+        }
+        List<DeployPipelinePhaseVo> combopPhaseList = pipelineConfigVo.getCombopPhaseList();
+        if (CollectionUtils.isEmpty(combopPhaseList)) {
+            return null;
+        }
+        Long phaseId = config.getLong("phaseId");
+        for (DeployPipelinePhaseVo combopPhaseVo : combopPhaseList) {
+            if (combopPhaseVo == null) {
+                continue;
+            }
+            if (moduleId != null) {
+                //如果是模块层或环境层，没有重载，就不用保存依赖关系
+                Integer override = combopPhaseVo.getOverride();
+                if (Objects.equals(override, 0)) {
+                    continue;
+                }
+            }
+            if (!Objects.equals(combopPhaseVo.getId(), phaseId)) {
+                continue;
+            }
+            AutoexecCombopPhaseConfigVo phaseConfigVo = combopPhaseVo.getConfig();
+            if (phaseConfigVo == null) {
+                return null;
+            }
+            List<AutoexecCombopPhaseOperationVo> phaseOperationList = phaseConfigVo.getPhaseOperationList();
+            if (CollectionUtils.isEmpty(phaseOperationList)) {
+                return null;
+            }
+            Long operationId = Long.getLong(dependencyVo.getTo());
+            for (AutoexecCombopPhaseOperationVo phaseOperationVo : phaseOperationList) {
+                if (phaseOperationVo == null) {
+                    continue;
+                }
+                if (!Objects.equals(phaseOperationVo.getOperationId(), operationId)) {
+                    continue;
+                }
+                AutoexecCombopPhaseOperationConfigVo operationConfigVo = phaseOperationVo.getConfig();
+                if (operationConfigVo == null) {
+                    return null;
+                }
+            }
+        }
+        return null;
     }
 
     @Override
-    protected DependencyInfoVo parse(DependencyVo dependencyObj) {
-        return null;
+    public IFromType getFromType() {
+        return AutoexecFromType.GLOBAL_PARAM;
     }
 }
