f7ff3904b088ca8aa95127b9cd1e8112b7d6df2c
linbq@techsure.com.cn <Besiter789>
2022-06-20 18:04:18 +0800

    --task=1006490 --user=林邦泉 后端-应用配置-流水线作业参数保存与获取接口 https://www.tapd.cn/54247054/s/1236466

--- src/main/java/codedriver/module/deploy/api/pipeline/DeployAppPipelineParamSaveApi.java
+++ src/main/java/codedriver/module/deploy/api/pipeline/DeployAppPipelineParamSaveApi.java
@@ -8,15 +8,15 @@ package codedriver.module.deploy.api.pipeline;
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.autoexec.auth.AUTOEXEC_BASE;
 import codedriver.framework.autoexec.constvalue.ParamType;
+import codedriver.framework.autoexec.crossover.IAutoexecServiceCrossoverService;
 import codedriver.framework.autoexec.dto.AutoexecParamVo;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.constvalue.CiphertextPrefix;
 import codedriver.framework.common.util.RC4Util;
+import codedriver.framework.crossover.CrossoverServiceFactory;
 import codedriver.framework.dependency.core.DependencyManager;
 import codedriver.framework.deploy.dto.app.DeployAppConfigVo;
 import codedriver.framework.deploy.dto.app.DeployPipelineConfigVo;
-import codedriver.framework.exception.type.ParamIrregularException;
-import codedriver.framework.exception.type.ParamNotExistsException;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
@@ -24,7 +24,6 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.deploy.dao.mapper.DeployAppConfigMapper;
-import codedriver.module.deploy.dependency.handler.AutoexecProfile2DeployAppPipelinePhaseOperationDependencyHandler;
 import codedriver.module.deploy.dependency.handler.Matrix2DeployAppPipelineParamDependencyHandler;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -36,7 +35,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.regex.Pattern;
 
 @Service
 @Transactional
@@ -83,43 +81,18 @@ public class DeployAppPipelineParamSaveApi extends PrivateApiComponentBase {
                 }
             }
         }
-        Pattern keyPattern = Pattern.compile("^[A-Za-z_\\d]+$");
-        Pattern namePattern = Pattern.compile("^[A-Za-z_\\d\\u4e00-\\u9fa5]+$");
         JSONArray paramList = jsonObj.getJSONArray("paramList");
         if (CollectionUtils.isEmpty(paramList)) {
             return null;
         }
         List<AutoexecParamVo> runtimeParamList = paramList.toJavaList(AutoexecParamVo.class);
+        IAutoexecServiceCrossoverService autoexecServiceCrossoverService = CrossoverServiceFactory.getApi(IAutoexecServiceCrossoverService.class);
+        autoexecServiceCrossoverService.validateRuntimeParamList(runtimeParamList);
         for (int i = 0; i < runtimeParamList.size(); i++) {
             AutoexecParamVo autoexecParamVo = runtimeParamList.get(i);
             if (autoexecParamVo != null) {
-                String key = autoexecParamVo.getKey();
-                int index = i + 1;
-                if (StringUtils.isBlank(key)) {
-                    throw new ParamNotExistsException(index, "英文名");
-                }
-                if (!keyPattern.matcher(key).matches()) {
-                    throw new ParamIrregularException(key);
-                }
-                String name = autoexecParamVo.getName();
-                if (StringUtils.isBlank(name)) {
-                    throw new ParamNotExistsException(index, key, "中文名");
-                }
-                if (!namePattern.matcher(name).matches()) {
-                    throw new ParamIrregularException(index, key, name);
-                }
-                Integer isRequired = autoexecParamVo.getIsRequired();
-                if (isRequired == null) {
-                    throw new ParamNotExistsException(index, key, "是否必填");
-                }
                 String type = autoexecParamVo.getType();
-                if (StringUtils.isBlank(type)) {
-                    throw new ParamNotExistsException(index, key, "控件类型");
-                }
                 ParamType paramType = ParamType.getParamType(type);
-                if (paramType == null) {
-                    throw new ParamIrregularException(index, key, type);
-                }
                 Object value = autoexecParamVo.getDefaultValue();
                 // 如果默认值不以"RC4:"开头，说明修改了密码，则重新加密
                 if (paramType == ParamType.PASSWORD && value != null && !value.toString().startsWith(CiphertextPrefix.RC4.getValue())) {
