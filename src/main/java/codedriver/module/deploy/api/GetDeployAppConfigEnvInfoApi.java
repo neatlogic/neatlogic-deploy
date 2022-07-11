6b12a7790d017173b2aa1d6ba2cba7becd007eca
linbq@techsure.com.cn <Besiter789>
2022-07-11 18:40:20 +0800

    --task=1006566 --user=林邦泉 CMDB资产清单性能优化-重构代码 https://www.tapd.cn/54247054/s/1240228

--- src/main/java/codedriver/module/deploy/api/GetDeployAppConfigEnvInfoApi.java
+++ src/main/java/codedriver/module/deploy/api/GetDeployAppConfigEnvInfoApi.java
@@ -5,29 +5,18 @@
 
 package codedriver.module.deploy.api;
 
-import codedriver.framework.asynchronization.threadlocal.TenantContext;
-import codedriver.framework.cmdb.crossover.IResourceCenterCommonGenerateSqlCrossoverService;
 import codedriver.framework.cmdb.dao.mapper.resourcecenter.ResourceCenterMapper;
-import codedriver.framework.cmdb.dto.resourcecenter.ResourceVo;
-import codedriver.framework.cmdb.dto.resourcecenter.config.ResourceInfo;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.dto.BasePageVo;
-import codedriver.framework.crossover.CrossoverServiceFactory;
-import codedriver.framework.deploy.dto.app.DeployAppConfigEnvDBConfigVo;
-import codedriver.framework.deploy.dto.app.DeployAppEnvAutoConfigKeyValueVo;
 import codedriver.framework.deploy.dto.app.DeployAppEnvAutoConfigVo;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.deploy.dao.mapper.DeployAppConfigMapper;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author lvzk
@@ -71,40 +60,40 @@ public class GetDeployAppConfigEnvInfoApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) {
         JSONObject envInfo = new JSONObject();
-        DeployAppEnvAutoConfigVo envAutoConfigVo = paramObj.toJavaObject(DeployAppEnvAutoConfigVo.class);
-
-        //获取环境 autoConfig
-        List<DeployAppEnvAutoConfigKeyValueVo> envAutoConfigList = deployAppConfigMapper.getAppEnvAutoConfigKeyValueList(envAutoConfigVo);
-        envInfo.put("envAutoConfigList", envAutoConfigList);
-
-        //TODO 根据appSystemId获取阶段信息
-
-        //获取实例列表
-        List<Long> instanceIdList = resourceCenterMapper.getResourceIdListByAppSystemIdAndModuleIdAndEnvId(paramObj.toJavaObject(ResourceVo.class), TenantContext.get().getDataDbName());
-
-        //获取实例autoConfig
-        if (CollectionUtils.isNotEmpty(instanceIdList)) {
-            List<ResourceVo> instanceList = new ArrayList<>();
-            List<ResourceInfo> unavailableResourceInfoList = new ArrayList<>();
-            List<ResourceInfo> theadList = new ArrayList<>();
-            theadList.add(new ResourceInfo("resource_ipobject", "id"));
-            theadList.add(new ResourceInfo("resource_ipobject", "name"));
-            theadList.add(new ResourceInfo("resource_ipobject", "ip"));
-            theadList.add(new ResourceInfo("resource_softwareservice", "port"));
-            theadList.add(new ResourceInfo("resource_ipobject", "maintenance_window"));
-            IResourceCenterCommonGenerateSqlCrossoverService resourceCenterCrossoverService = CrossoverServiceFactory.getApi(IResourceCenterCommonGenerateSqlCrossoverService.class);
-            String sql = resourceCenterCrossoverService.getResourceListByIdListSql("resource_ipobject", theadList, instanceIdList, unavailableResourceInfoList);
-            if (StringUtils.isNotBlank(sql)) {
-                instanceList = resourceCenterCrossoverService.getResourceList(sql);
-            }
-//            List<ResourceVo> instanceList = resourceCenterMapper.getResourceListByIdList(instanceIdList, TenantContext.get().getDataDbName());
-            envInfo.put("instanceList", instanceList);
-            List<DeployAppEnvAutoConfigVo> instanceAutoConfigList = deployAppConfigMapper.getAppEnvAutoConfigListBySystemIdAndModuleIdAndEnvIdAndInstanceIdList(paramObj.getLong("appSystemId"), paramObj.getLong("appModuleId"), paramObj.getLong("envId"), instanceIdList);
-            envInfo.put("instanceAutoConfigList", instanceAutoConfigList);
-        }
-        //DB配置
-        List<DeployAppConfigEnvDBConfigVo> appConfigEnvDBConfigList = deployAppConfigMapper.getAppConfigEnvDBConfigListByAppSystemIdAndAppModuleIdAndEnvId(paramObj.getLong("appSystemId"), paramObj.getLong("appModuleId"), paramObj.getLong("envId"));
-        envInfo.put("DBConfigList", appConfigEnvDBConfigList);
+//        DeployAppEnvAutoConfigVo envAutoConfigVo = paramObj.toJavaObject(DeployAppEnvAutoConfigVo.class);
+//
+//        //获取环境 autoConfig
+//        List<DeployAppEnvAutoConfigKeyValueVo> envAutoConfigList = deployAppConfigMapper.getAppEnvAutoConfigKeyValueList(envAutoConfigVo);
+//        envInfo.put("envAutoConfigList", envAutoConfigList);
+//
+//        //TODO 根据appSystemId获取阶段信息
+//
+//        //获取实例列表
+//        List<Long> instanceIdList = resourceCenterMapper.getResourceIdListByAppSystemIdAndModuleIdAndEnvId(paramObj.toJavaObject(ResourceVo.class), TenantContext.get().getDataDbName());
+//
+//        //获取实例autoConfig
+//        if (CollectionUtils.isNotEmpty(instanceIdList)) {
+//            List<ResourceVo> instanceList = new ArrayList<>();
+//            List<ResourceInfo> unavailableResourceInfoList = new ArrayList<>();
+//            List<ResourceInfo> theadList = new ArrayList<>();
+//            theadList.add(new ResourceInfo("resource_ipobject", "id"));
+//            theadList.add(new ResourceInfo("resource_ipobject", "name"));
+//            theadList.add(new ResourceInfo("resource_ipobject", "ip"));
+//            theadList.add(new ResourceInfo("resource_softwareservice", "port"));
+//            theadList.add(new ResourceInfo("resource_ipobject", "maintenance_window"));
+//            IResourceCenterCommonGenerateSqlCrossoverService resourceCenterCrossoverService = CrossoverServiceFactory.getApi(IResourceCenterCommonGenerateSqlCrossoverService.class);
+//            String sql = resourceCenterCrossoverService.getResourceListByIdListSql("resource_ipobject", theadList, instanceIdList, unavailableResourceInfoList);
+//            if (StringUtils.isNotBlank(sql)) {
+//                instanceList = resourceCenterCrossoverService.getResourceList(sql);
+//            }
+////            List<ResourceVo> instanceList = resourceCenterMapper.getResourceListByIdList(instanceIdList, TenantContext.get().getDataDbName());
+//            envInfo.put("instanceList", instanceList);
+//            List<DeployAppEnvAutoConfigVo> instanceAutoConfigList = deployAppConfigMapper.getAppEnvAutoConfigListBySystemIdAndModuleIdAndEnvIdAndInstanceIdList(paramObj.getLong("appSystemId"), paramObj.getLong("appModuleId"), paramObj.getLong("envId"), instanceIdList);
+//            envInfo.put("instanceAutoConfigList", instanceAutoConfigList);
+//        }
+//        //DB配置
+//        List<DeployAppConfigEnvDBConfigVo> appConfigEnvDBConfigList = deployAppConfigMapper.getAppConfigEnvDBConfigListByAppSystemIdAndAppModuleIdAndEnvId(paramObj.getLong("appSystemId"), paramObj.getLong("appModuleId"), paramObj.getLong("envId"));
+//        envInfo.put("DBConfigList", appConfigEnvDBConfigList);
 
         return envInfo;
     }
