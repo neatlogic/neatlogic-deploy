f60c0333fe1d55da0f3bce750b3b4662879b079c
赖文韬 <laiwt@techsure.com.cn>
2022-07-11 17:37:53 +0800

    自动发布对外接口-getVer

--- src/main/java/codedriver/module/deploy/api/version/GetDeployVersionConfigApi.java
+++ src/main/java/codedriver/module/deploy/api/version/GetDeployVersionConfigApi.java
@@ -2,7 +2,7 @@ package codedriver.module.deploy.api.version;
 
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.deploy.auth.DEPLOY_MODIFY;
+import codedriver.framework.deploy.auth.DEPLOY_BASE;
 import codedriver.framework.deploy.dto.version.DeployVersionVo;
 import codedriver.framework.deploy.exception.DeployVersionNotFoundException;
 import codedriver.framework.restful.annotation.Description;
@@ -13,28 +13,26 @@ import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.deploy.dao.mapper.DeployVersionMapper;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.collections4.MapUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Map;
 
 @Service
-@AuthAction(action = DEPLOY_MODIFY.class)
-@OperationType(type = OperationTypeEnum.UPDATE)
-public class UpdateDeployVersionConfigApi extends PrivateApiComponentBase {
+@AuthAction(action = DEPLOY_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class GetDeployVersionConfigApi extends PrivateApiComponentBase {
 
     @Resource
     DeployVersionMapper deployVersionMapper;
 
     @Override
     public String getName() {
-        return "更新发布版本配置";
+        return "获取发布版本配置";
     }
 
     @Override
     public String getToken() {
-        return "deploy/version/config/update";
+        return "deploy/version/config/get";
     }
 
     @Override
@@ -46,31 +44,18 @@ public class UpdateDeployVersionConfigApi extends PrivateApiComponentBase {
             @Param(name = "sysId", desc = "应用ID", isRequired = true, type = ApiParamType.LONG),
             @Param(name = "moduleId", desc = "应用系统id", isRequired = true, type = ApiParamType.LONG),
             @Param(name = "version", desc = "版本号", isRequired = true, type = ApiParamType.STRING),
-            @Param(name = "verInfo", desc = "版本信息", type = ApiParamType.JSONOBJECT),
     })
-    @Description(desc = "更新发布版本配置")
+    @Description(desc = "获取发布版本配置")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         Long sysId = paramObj.getLong("sysId");
         Long moduleId = paramObj.getLong("moduleId");
         String version = paramObj.getString("version");
-        JSONObject verInfo = paramObj.getJSONObject("verInfo");
         DeployVersionVo versionVo = deployVersionMapper.getDeployVersionBySystemIdAndModuleIdAndVersion(new DeployVersionVo(version, sysId, moduleId));
         if (versionVo == null) {
             throw new DeployVersionNotFoundException(version);
         }
-        JSONObject config = versionVo.getConfig();
-        if (MapUtils.isNotEmpty(verInfo)) {
-            if (MapUtils.isNotEmpty(config)) {
-                for (Map.Entry<String, Object> info : verInfo.entrySet()) {
-                    config.put(info.getKey(), info.getValue());
-                }
-            } else {
-                config = verInfo;
-            }
-            deployVersionMapper.updateDeployVersionConfig(new DeployVersionVo(versionVo.getId(), config));
-        }
-        return null;
+        return versionVo.getConfig();
     }
 
 }
