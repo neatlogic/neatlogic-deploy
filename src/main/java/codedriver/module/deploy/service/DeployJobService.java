3c082ba9d06ad2f0b6ec3f9a522412857b3cfee3
陈其炜 <chenqw@techsure.com.cn>
2022-07-26 15:50:10 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/deploy/service/DeployJobService.java
+++ src/main/java/codedriver/module/deploy/service/DeployJobService.java
@@ -1,13 +1,18 @@
 /*
- * Copyright (c)  2022 TechSure Co.,Ltd.  All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
 package codedriver.module.deploy.service;
 
+import codedriver.framework.deploy.dto.job.DeployJobVo;
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.List;
+
 public interface DeployJobService {
+
+    List<DeployJobVo> searchDeployJob(DeployJobVo deployJobVo);
 
     /**
      * 校验&&补充作业参数
