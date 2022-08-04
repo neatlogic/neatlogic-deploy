4261653eb989e2166fa8ec7a86e8c17b07f18d9e
lvzk <lvzk@techsure.com.cn>
2022-08-04 18:57:59 +0800

    batch deploy job

--- src/main/java/codedriver/module/deploy/job/callback/BatchJobCallbackHandler.java
+++ src/main/java/codedriver/module/deploy/job/callback/BatchJobCallbackHandler.java
@@ -10,31 +10,45 @@ import codedriver.framework.autoexec.dao.mapper.AutoexecJobMapper;
 import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
 import codedriver.framework.autoexec.job.callback.core.AutoexecJobCallbackBase;
 import codedriver.framework.deploy.constvalue.JobSource;
+import codedriver.framework.deploy.dto.job.LaneGroupVo;
+import codedriver.module.deploy.dao.mapper.DeployBatchJobMapper;
+import codedriver.module.deploy.service.DeployBatchJobService;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
  * @author lvzk
  * @since 2022/7/27 17:40
  **/
+@Component
 public class BatchJobCallbackHandler extends AutoexecJobCallbackBase {
     @Resource
     private AutoexecJobMapper autoexecJobMapper;
+    @Resource
+    private DeployBatchJobService deployBatchJobService;
+    @Resource
+    private DeployBatchJobMapper deployBatchJobMapper;
 
     @Override
     public String getHandler() {
-        return null;
+        return BatchJobCallbackHandler.class.getSimpleName();
     }
 
     @Override
     public Boolean getIsNeedCallback(AutoexecJobVo jobVo) {
         if (jobVo != null) {
             AutoexecJobVo autoexecJob = autoexecJobMapper.getJobInfo(jobVo.getId());
-            if (Objects.equals(JobSource.DEPLOY.getValue(),autoexecJob.getSource())) {
-                if (!JobStatus.PENDING.getValue().equals(jobVo.getStatus()) && !JobStatus.RUNNING.getValue().equals(jobVo.getStatus())) {
-                    return true;
+            if (Objects.equals(JobSource.DEPLOY.getValue(), autoexecJob.getSource()) && autoexecJob.getParentId() != null) {
+                //作业回调
+                AutoexecJobVo parentJobVo = autoexecJobMapper.getJobInfo(autoexecJob.getParentId());
+                if (parentJobVo != null && Objects.equals(parentJobVo.getSource(), JobSource.BATCHDEPLOY.getValue())) {
+                    return Arrays.asList(JobStatus.COMPLETED.getValue(),JobStatus.FAILED.getValue(),JobStatus.ABORTED.getValue()).contains(autoexecJob.getStatus()) ;
                 }
+            } else if (Objects.equals(autoexecJob.getSource(), JobSource.BATCHDEPLOY.getValue())) {
+                //TODO 批量作业回调
             }
         }
         return false;
@@ -42,6 +56,10 @@ public class BatchJobCallbackHandler extends AutoexecJobCallbackBase {
 
     @Override
     public void doService(Long invokeId, AutoexecJobVo jobVo) {
-
+        Long jobId = jobVo.getId();
+        LaneGroupVo laneGroupVo = deployBatchJobMapper.getLaneGroupByJobId(jobId);
+        if(laneGroupVo != null) {
+            deployBatchJobService.checkAndFireLaneNextGroup(laneGroupVo.getId());
+        }
     }
 }
