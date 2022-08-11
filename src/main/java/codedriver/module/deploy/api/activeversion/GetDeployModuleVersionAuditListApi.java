36dc64c3d7894e6e43ad989ba42becd8d44ef397
赖文韬 <laiwt@techsure.com.cn>
2022-08-11 11:59:24 +0800

    --story=1006699 --user=赖文韬 自动发布-活动版本 https://www.tapd.cn/54247054/s/1245813

---
+++ src/main/java/codedriver/module/deploy/api/activeversion/GetDeployModuleVersionAuditListApi.java
@@ -0,0 +1,79 @@
+package codedriver.module.deploy.api.activeversion;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.deploy.auth.DEPLOY_BASE;
+import codedriver.framework.deploy.dto.env.DeployEnvVersionAuditVo;
+import codedriver.framework.deploy.dto.version.DeployVersionVo;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.deploy.dao.mapper.DeployAppConfigMapper;
+import codedriver.module.deploy.dao.mapper.DeployEnvVersionMapper;
+import codedriver.module.deploy.dao.mapper.DeployVersionMapper;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AuthAction(action = DEPLOY_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class GetDeployModuleVersionAuditListApi extends PrivateApiComponentBase {
+
+    @Resource
+    DeployVersionMapper deployVersionMapper;
+
+    @Resource
+    DeployAppConfigMapper deployAppConfigMapper;
+
+    @Resource
+    DeployEnvVersionMapper deployEnvVersionMapper;
+
+    @Override
+    public String getName() {
+        return "获取模块版本历史";
+    }
+
+    @Override
+    public String getToken() {
+        return "deploy/module/version/audit/list";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({
+            @Param(name = "appSystemId", type = ApiParamType.LONG, isRequired = true, desc = "应用 id"),
+            @Param(name = "appModuleId", type = ApiParamType.LONG, isRequired = true, desc = "模块 id"),
+            @Param(name = "envId", type = ApiParamType.LONG, isRequired = true, desc = "环境 id"),
+    })
+    @Output({
+    })
+    @Description(desc = "获取模块版本历史")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        Long appSystemId = paramObj.getLong("appSystemId");
+        Long appModuleId = paramObj.getLong("appModuleId");
+        Long envId = paramObj.getLong("envId");
+        List<DeployEnvVersionAuditVo> auditList = deployEnvVersionMapper.getDeployEnvVersionAuditBySystemIdAndModueIdAndEnvId(appSystemId, appModuleId, envId);
+        if (auditList.size() > 0) {
+            Set<Long> versionIdSet = auditList.stream().map(DeployEnvVersionAuditVo::getNewVersionId).collect(Collectors.toSet());
+            versionIdSet.addAll(auditList.stream().map(DeployEnvVersionAuditVo::getOldVersionId).collect(Collectors.toSet()));
+            List<DeployVersionVo> versionList = deployVersionMapper.getDeployVersionByIdList(new ArrayList<>(versionIdSet));
+            Map<Long, String> versionMap = versionList.stream().collect(Collectors.toMap(DeployVersionVo::getId, DeployVersionVo::getVersion));
+            for (DeployEnvVersionAuditVo vo : auditList) {
+                vo.setNewVersion(versionMap.get(vo.getNewVersionId()));
+                vo.setOldVersion(versionMap.get(vo.getOldVersionId()));
+            }
+        }
+        return auditList;
+    }
+}
