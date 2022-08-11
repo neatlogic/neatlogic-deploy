2cec44be1c8b3dab407d162336f3c3ab5b11270f
陈其炜 <chenqw@techsure.com.cn>
2022-08-11 11:58:17 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

---
+++ src/main/java/codedriver/module/deploy/api/job/batch/GetBatchDeployJobStatusApi.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.deploy.api.job.batch;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.deploy.auth.DEPLOY_BASE;
+import codedriver.framework.deploy.dto.job.DeployJobVo;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.deploy.dao.mapper.DeployJobMapper;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+
+@Service
+@OperationType(type = OperationTypeEnum.SEARCH)
+@AuthAction(action = DEPLOY_BASE.class)
+public class GetBatchDeployJobStatusApi extends PrivateApiComponentBase {
+
+    @Resource
+    DeployJobMapper deployJobMapper;
+
+    @Override
+    public String getName() {
+        return "获取单个批量作业状态信息";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({@Param(name = "id", type = ApiParamType.LONG, isRequired = true, desc = "作业id")})
+    @Output({@Param(explode = DeployJobVo.class)})
+    @Description(desc = "获取单个批量作业状态信息接口，主要用于刷新状态")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        return deployJobMapper.getBatchDeployJobStatusById(jsonObj.getLong("id"));
+    }
+
+    @Override
+    public String getToken() {
+        return "/deploy/batchjob/status/get";
+    }
+}
