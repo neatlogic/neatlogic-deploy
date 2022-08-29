120fe19f6eccec13c6e514440ceca2e28c7473b8
赖文韬 <laiwt@techsure.com.cn>
2022-08-29 18:42:55 +0800

    --story=1006823 --user=赖文韬 自动化/自动发布-作业详情页面内，非runner阶段，需要增加【节点导出】功能 https://www.tapd.cn/54247054/s/1249861

--- src/main/java/codedriver/module/deploy/job/source/type/DeployJobSourceTypeHandler.java
+++ src/main/java/codedriver/module/deploy/job/source/type/DeployJobSourceTypeHandler.java
@@ -11,6 +11,7 @@ import codedriver.framework.auth.core.AuthActionChecker;
 import codedriver.framework.autoexec.constvalue.ExecMode;
 import codedriver.framework.autoexec.constvalue.JobNodeStatus;
 import codedriver.framework.autoexec.dao.mapper.AutoexecJobMapper;
+import codedriver.framework.autoexec.dto.ISqlDetail;
 import codedriver.framework.autoexec.dto.combop.AutoexecCombopPhaseVo;
 import codedriver.framework.autoexec.dto.combop.AutoexecCombopVo;
 import codedriver.framework.autoexec.dto.job.AutoexecJobPhaseNodeVo;
@@ -51,9 +52,6 @@ import codedriver.framework.exception.type.ParamIrregularException;
 import codedriver.framework.integration.authentication.enums.AuthenticateType;
 import codedriver.framework.util.HttpRequestUtil;
 import codedriver.framework.util.TableResultUtil;
-import codedriver.framework.util.TimeUtil;
-import codedriver.framework.util.excel.ExcelBuilder;
-import codedriver.framework.util.excel.SheetBuilder;
 import codedriver.module.deploy.auth.core.DeployAppAuthChecker;
 import codedriver.module.deploy.dao.mapper.DeployAppConfigMapper;
 import codedriver.module.deploy.dao.mapper.DeployJobMapper;
@@ -62,19 +60,11 @@ import codedriver.module.deploy.dao.mapper.DeployVersionMapper;
 import codedriver.module.deploy.util.DeployPipelineConfigManager;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.JSONReader;
 import com.alibaba.nacos.common.utils.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -458,105 +448,26 @@ public class DeployJobSourceTypeHandler extends AutoexecJobSourceTypeHandlerBase
                 jobVo.setIsCanExecute(1);
             } else {
                 jobVo.setIsCanTakeOver(1);
+                jobVo.setIsCanTakeOver(1);
             }
         }
     }
 
     @Override
-    public Workbook exportJobPhaseSql(AutoexecJobPhaseNodeVo jobPhaseNodeVo, AutoexecJobVo jobVo, AutoexecJobPhaseVo phaseVo, List<String> headList, List<String> columnList) {
-        Workbook workbook = null;
+    protected int getJobPhaseSqlCount(AutoexecJobPhaseNodeVo jobPhaseNodeVo) {
         jobPhaseNodeVo.setJobPhaseName(autoexecJobMapper.getJobPhaseByPhaseId(jobPhaseNodeVo.getJobPhaseId()).getName());
-        int sqlCount = deploySqlMapper.searchDeploySqlCount(jobPhaseNodeVo);
-        if (sqlCount > 0) {
-            List<DeploySqlDetailVo> list;
-            jobPhaseNodeVo.setRowNum(sqlCount);
-            ExcelBuilder builder = new ExcelBuilder(SXSSFWorkbook.class);
-            SheetBuilder sheetBuilder = builder.withBorderColor(HSSFColor.HSSFColorPredefined.GREY_40_PERCENT)
-                    .withHeadFontColor(HSSFColor.HSSFColorPredefined.WHITE)
-                    .withHeadBgColor(HSSFColor.HSSFColorPredefined.DARK_BLUE)
-                    .withColumnWidth(30)
-                    .addSheet("数据")
-                    .withHeaderList(headList)
-                    .withColumnList(columnList);
-            workbook = builder.build();
-            jobPhaseNodeVo.setPageSize(20);
-            Integer pageCount = jobPhaseNodeVo.getPageCount();
-            for (int i = 1; i <= pageCount; i++) {
-                jobPhaseNodeVo.setCurrentPage(i);
-                list = deploySqlMapper.searchDeploySql(jobPhaseNodeVo);
-                Map<Long, Map<String, Object>> nodeDataMap = new LinkedHashMap<>();
-                Map<String, List<Long>> runnerNodeMap = new HashMap<>();
-                Map<Long, JSONObject> nodeLogTailParamMap = new HashMap<>();
-                for (DeploySqlDetailVo vo : list) {
-                    Map<String, Object> dataMap = new HashMap<>();
-                    dataMap.put("name", vo.getSqlFile());
-                    dataMap.put("host", vo.getHost() + (vo.getPort() != null ? ":" + vo.getPort() : ""));
-                    dataMap.put("nodeName", vo.getNodeName());
-                    dataMap.put("statusName", vo.getStatusName());
-                    dataMap.put("costTime", vo.getCostTime());
-                    dataMap.put("startTime", vo.getStartTime() != null ? TimeUtil.convertDateToString(vo.getStartTime(), TimeUtil.YYYY_MM_DD_HH_MM_SS) : "");
-                    dataMap.put("endTime", vo.getEndTime() != null ? TimeUtil.convertDateToString(vo.getEndTime(), TimeUtil.YYYY_MM_DD_HH_MM_SS) : "");
-                    nodeDataMap.put(vo.getId(), dataMap);
-                    runnerNodeMap.computeIfAbsent(vo.getRunnerUrl(), k -> new ArrayList<>()).add(vo.getId());
-                    nodeLogTailParamMap.put(vo.getId(), new JSONObject() {
-                        {
-                            this.put("id", vo.getId());
-                            this.put("jobId", jobVo.getId());
-                            this.put("resourceId", vo.getResourceId());
-                            this.put("sqlName", vo.getSqlFile());
-                            this.put("phase", phaseVo.getName());
-                            this.put("ip", vo.getHost());
-                            this.put("port", vo.getPort());
-                            this.put("execMode", phaseVo.getExecMode());
-                        }
-                    });
-                }
-                for (Map.Entry<String, List<Long>> entry : runnerNodeMap.entrySet()) {
-                    String url = entry.getKey() + "api/binary/job/phase/batchnode/log/tail";
-                    List<Long> value = entry.getValue();
-                    if (org.apache.commons.collections4.CollectionUtils.isNotEmpty(value)) {
-                        JSONArray nodeList = new JSONArray();
-                        value.forEach(o -> nodeList.add(nodeLogTailParamMap.get(o)));
-                        JSONObject paramJson = new JSONObject();
-                        paramJson.put("nodeList", nodeList);
-                        paramJson.put("wordCountLimit", 2048);
-                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                        HttpRequestUtil requestUtil = HttpRequestUtil.download(url, "POST", bos)
-                                .setPayload(paramJson.toJSONString())
-                                .setAuthType(AuthenticateType.BUILDIN)
-                                .setConnectTimeout(5000)
-                                .setReadTimeout(5000)
-                                .sendRequest();
-                        String error = requestUtil.getError();
-                        if (StringUtils.isNotBlank(error)) {
-                            continue;
-                        }
-                        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
-                        InputStreamReader isr = new InputStreamReader(bis);
-                        JSONReader jsonReader = new JSONReader(isr);
-                        jsonReader.startArray();
-                        while (jsonReader.hasNext()) {
-                            JSONObject nodeObj = jsonReader.readObject(JSONObject.class);
-                            Long id = nodeObj.getLong("id");
-                            String content = nodeObj.getString("content");
-                            Map<String, Object> map = nodeDataMap.get(id);
-                            if (map != null) {
-                                map.put("log", content);
-                            }
-                        }
-                        jsonReader.endArray();
-                        jsonReader.close();
-                        try {
-                            bis.close();
-                            bos.close();
-                        } catch (IOException e) {
-
-                        }
-                    }
-                }
-                nodeDataMap.values().forEach(sheetBuilder::addData);
-            }
-        }
-        return workbook;
+        return deploySqlMapper.searchDeploySqlCount(jobPhaseNodeVo);
     }
+
+    @Override
+    protected List<ISqlDetail> searchJobPhaseSqlForExpport(AutoexecJobPhaseNodeVo jobPhaseNodeVo) {
+        List<ISqlDetail> result = new ArrayList<>();
+        List<DeploySqlDetailVo> list = deploySqlMapper.searchDeploySql(jobPhaseNodeVo);
+        if (list.size() > 0) {
+            list.forEach(o -> result.add(o));
+        }
+        deploySqlMapper.searchDeploySql(jobPhaseNodeVo);
+        return result;
+    }
+
 }
