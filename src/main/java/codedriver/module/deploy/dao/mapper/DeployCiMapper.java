362f0fca4437e874fac96be150e2e7318417260c
赖文韬 <laiwt@techsure.com.cn>
2022-09-02 15:17:15 +0800

    --story=1006866 --user=赖文韬 [自动发布]-持续集成 https://www.tapd.cn/54247054/s/1250918

--- src/main/java/codedriver/module/deploy/dao/mapper/DeployCiMapper.java
+++ src/main/java/codedriver/module/deploy/dao/mapper/DeployCiMapper.java
@@ -2,11 +2,19 @@ package codedriver.module.deploy.dao.mapper;
 
 import codedriver.framework.deploy.dto.ci.DeployCiVo;
 
+import java.util.List;
+
 public interface DeployCiMapper {
 
     int checkDeployCiIsRepeat(DeployCiVo vo);
 
     DeployCiVo getDeployCiById(Long id);
+
+    int searchDeployCiCount(DeployCiVo vo);
+
+    List<Long> searchDeployCiIdList(DeployCiVo vo);
+
+    List<DeployCiVo> getDeployCiListByIdList(List<Long> list);
 
     int insertDeployCi(DeployCiVo vo);
 }
