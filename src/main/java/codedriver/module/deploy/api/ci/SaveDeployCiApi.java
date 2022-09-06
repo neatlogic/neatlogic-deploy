26b9f4473b469c0d206ae9c0f0b2803ecb480afd
赖文韬 <laiwt@techsure.com.cn>
2022-09-06 18:12:44 +0800

    --story=1006866 --user=赖文韬 [自动发布]-持续集成 https://www.tapd.cn/54247054/s/1251121

--- src/main/java/codedriver/module/deploy/api/ci/SaveDeployCiApi.java
+++ src/main/java/codedriver/module/deploy/api/ci/SaveDeployCiApi.java
@@ -2,17 +2,20 @@ package codedriver.module.deploy.api.ci;
 
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.cmdb.crossover.ICiEntityCrossoverMapper;
+import codedriver.framework.cmdb.dto.cientity.CiEntityVo;
 import codedriver.framework.cmdb.exception.cientity.CiEntityNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.crossover.CrossoverServiceFactory;
 import codedriver.framework.deploy.auth.DEPLOY_MODIFY;
-import codedriver.framework.deploy.constvalue.DeployCiActionType;
-import codedriver.framework.deploy.constvalue.DeployCiTriggerType;
-import codedriver.framework.deploy.constvalue.DeployCiRepoEvent;
-import codedriver.framework.deploy.constvalue.DeployCiRepoType;
+import codedriver.framework.deploy.constvalue.*;
 import codedriver.framework.deploy.dto.ci.DeployCiVo;
+import codedriver.framework.deploy.exception.DeployAppConfigModuleRunnerGroupNotFoundException;
+import codedriver.framework.deploy.exception.DeployCiGitlabAccountLostException;
+import codedriver.framework.deploy.exception.DeployCiGitlabWebHookSaveFailedException;
 import codedriver.framework.deploy.exception.DeployCiIsRepeatException;
 import codedriver.framework.dto.FieldValidResultVo;
+import codedriver.framework.dto.runner.RunnerGroupVo;
+import codedriver.framework.dto.runner.RunnerVo;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
@@ -20,20 +23,30 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.IValid;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.util.HttpRequestUtil;
 import codedriver.framework.util.RegexUtils;
+import codedriver.module.deploy.dao.mapper.DeployAppConfigMapper;
 import codedriver.module.deploy.dao.mapper.DeployCiMapper;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
+@Transactional
 @AuthAction(action = DEPLOY_MODIFY.class)
 @OperationType(type = OperationTypeEnum.OPERATE)
 public class SaveDeployCiApi extends PrivateApiComponentBase {
 
     @Resource
     DeployCiMapper deployCiMapper;
+
+    @Resource
+    DeployAppConfigMapper deployAppConfigMapper;
 
     @Override
     public String getName() {
@@ -74,18 +87,56 @@ public class SaveDeployCiApi extends PrivateApiComponentBase {
             throw new DeployCiIsRepeatException(deployCiVo.getName());
         }
         ICiEntityCrossoverMapper iCiEntityCrossoverMapper = CrossoverServiceFactory.getApi(ICiEntityCrossoverMapper.class);
-        if (iCiEntityCrossoverMapper.getCiEntityBaseInfoById(deployCiVo.getAppSystemId()) == null) {
+        CiEntityVo system = iCiEntityCrossoverMapper.getCiEntityBaseInfoById(deployCiVo.getAppSystemId());
+        if (system == null) {
             throw new CiEntityNotFoundException(deployCiVo.getAppSystemId());
         }
-        if (iCiEntityCrossoverMapper.getCiEntityBaseInfoById(deployCiVo.getAppModuleId()) == null) {
+        CiEntityVo module = iCiEntityCrossoverMapper.getCiEntityBaseInfoById(deployCiVo.getAppModuleId());
+        if (module == null) {
             throw new CiEntityNotFoundException(deployCiVo.getAppModuleId());
         }
+        DeployCiVo ci = deployCiMapper.getDeployCiById(deployCiVo.getId());
+        if (DeployCiRepoType.GITLAB.getValue().equals(deployCiVo.getRepoType())) {
+            RunnerGroupVo runnerGroupVo = deployAppConfigMapper.getAppModuleRunnerGroupByAppSystemIdAndModuleId(deployCiVo.getAppSystemId(), deployCiVo.getAppModuleId());
+            if (runnerGroupVo == null || CollectionUtils.isEmpty(runnerGroupVo.getRunnerList())) {
+                throw new DeployAppConfigModuleRunnerGroupNotFoundException(system.getName() + "(" + deployCiVo.getAppSystemId() + ")", module.getName() + "(" + deployCiVo.getAppModuleId() + ")");
+            }
+            List<RunnerVo> runnerList = runnerGroupVo.getRunnerList();
+            int runnerMapIndex = (int) (Math.random() * runnerList.size());
+            RunnerVo runnerVo = runnerList.get(runnerMapIndex);
+
+            String gitlabUsername = deployCiVo.getConfig().getString("gitlabUsername");
+            String gitlabPassword = deployCiVo.getConfig().getString("gitlabPassword");
+            if (StringUtils.isBlank(gitlabUsername) || StringUtils.isBlank(gitlabPassword)) {
+                throw new DeployCiGitlabAccountLostException();
+            }
+            JSONObject param = new JSONObject();
+            param.put("ciId", deployCiVo.getId());
+            if (ci != null) {
+                param.put("hookId", deployCiVo.getHookId());
+            }
+            param.put("repoServerAddress", deployCiVo.getRepoServerAddress());
+            param.put("repoName", deployCiVo.getRepoName());
+            param.put("branchFilter", deployCiVo.getBranchFilter());
+            param.put("event", deployCiVo.getEvent());
+            param.put("authMode", DeployCiGitlabAuthMode.ACCESS_TOKEN.getValue());
+            param.put("username", gitlabUsername);
+            param.put("password", gitlabPassword);
+            HttpRequestUtil request = HttpRequestUtil.post(runnerVo.getUrl() + "/deploy/ci/gitlabwebhook/save").setPayload(param.toJSONString()).sendRequest();
+            String error = request.getError();
+            JSONObject resultJson = request.getResultJson();
+            if (StringUtils.isNotBlank(error)) {
+                throw new DeployCiGitlabWebHookSaveFailedException();
+            }
+            if (ci == null) {
+                deployCiVo.setHookId(resultJson.getString("Return"));
+            }
+        }
         deployCiMapper.insertDeployCi(deployCiVo);
-        // todo 生成webhook
         return null;
     }
 
-    public IValid version() {
+    public IValid name() {
         return value -> {
             DeployCiVo deployCiVo = value.toJavaObject(DeployCiVo.class);
             if (deployCiMapper.checkDeployCiIsRepeat(deployCiVo) > 0) {
