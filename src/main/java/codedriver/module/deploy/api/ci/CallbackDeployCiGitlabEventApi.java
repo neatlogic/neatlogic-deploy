21248f94bf5cfd17aac605eead80f3bff01b70cc
赖文韬 <laiwt@techsure.com.cn>
2022-09-15 17:20:02 +0800

    --story=1006866 --user=赖文韬 [自动发布]-持续集成 https://www.tapd.cn/54247054/s/1252298

--- src/main/java/codedriver/module/deploy/api/ci/CallbackDeployCiGitlabEventApi.java
+++ src/main/java/codedriver/module/deploy/api/ci/CallbackDeployCiGitlabEventApi.java
@@ -1,6 +1,10 @@
 package codedriver.module.deploy.api.ci;
 
+import codedriver.framework.asynchronization.threadlocal.TenantContext;
 import codedriver.framework.asynchronization.threadlocal.UserContext;
+import codedriver.framework.autoexec.constvalue.JobStatus;
+import codedriver.framework.autoexec.constvalue.JobTriggerType;
+import codedriver.framework.autoexec.constvalue.ReviewStatus;
 import codedriver.framework.autoexec.dto.combop.AutoexecCombopScenarioVo;
 import codedriver.framework.autoexec.dto.node.AutoexecNodeVo;
 import codedriver.framework.autoexec.exception.AutoexecScenarioIsNotFoundException;
@@ -8,6 +12,7 @@ import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.constvalue.SystemUser;
 import codedriver.framework.deploy.constvalue.DeployCiActionType;
 import codedriver.framework.deploy.constvalue.DeployCiTriggerType;
+import codedriver.framework.deploy.constvalue.JobSource;
 import codedriver.framework.deploy.dto.app.DeployPipelineConfigVo;
 import codedriver.framework.deploy.dto.ci.DeployCiVo;
 import codedriver.framework.deploy.dto.job.DeployJobModuleVo;
@@ -16,6 +21,7 @@ import codedriver.framework.deploy.dto.pipeline.PipelineGroupVo;
 import codedriver.framework.deploy.dto.pipeline.PipelineJobTemplateVo;
 import codedriver.framework.deploy.dto.pipeline.PipelineLaneVo;
 import codedriver.framework.deploy.dto.pipeline.PipelineVo;
+import codedriver.framework.deploy.dto.version.DeploySystemModuleVersionVo;
 import codedriver.framework.deploy.dto.version.DeployVersionVo;
 import codedriver.framework.deploy.exception.*;
 import codedriver.framework.filter.core.LoginAuthHandlerBase;
@@ -26,10 +32,17 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.ApiAnonymousAccessSupportEnum;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.scheduler.core.IJob;
+import codedriver.framework.scheduler.core.SchedulerManager;
+import codedriver.framework.scheduler.dto.JobObject;
+import codedriver.framework.scheduler.exception.ScheduleHandlerNotFoundException;
 import codedriver.framework.util.TimeUtil;
 import codedriver.module.deploy.dao.mapper.DeployCiMapper;
-import codedriver.module.deploy.dao.mapper.DeployVersionMapper;
+import codedriver.module.deploy.dao.mapper.DeployJobMapper;
 import codedriver.module.deploy.dao.mapper.DeployPipelineMapper;
+import codedriver.module.deploy.dao.mapper.DeployVersionMapper;
+import codedriver.module.deploy.schedule.plugin.DeployBatchJobAutoFireJob;
+import codedriver.module.deploy.service.DeployBatchJobService;
 import codedriver.module.deploy.service.DeployJobService;
 import codedriver.module.deploy.util.DeployPipelineConfigManager;
 import com.alibaba.fastjson.JSONArray;
@@ -66,10 +79,16 @@ public class CallbackDeployCiGitlabEventApi extends PrivateApiComponentBase {
     DeployVersionMapper deployVersionMapper;
 
     @Resource
-    private DeployPipelineMapper deployPipelineMapper;
+    DeployPipelineMapper deployPipelineMapper;
+
+    @Resource
+    DeployJobMapper deployJobMapper;
 
     @Resource
     DeployJobService deployJobService;
+
+    @Resource
+    DeployBatchJobService deployBatchJobService;
 
     @Override
     public String getName() {
@@ -97,7 +116,7 @@ public class CallbackDeployCiGitlabEventApi extends PrivateApiComponentBase {
     @Description(desc = "gitlab webhook回调api")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        /* todo
+        /*
             1、查出ciId对应的配置
             2、获取参数中的commits和ref，确定commitId、分支
             3、根据版本号规则拼接版本号
@@ -120,19 +139,35 @@ public class CallbackDeployCiGitlabEventApi extends PrivateApiComponentBase {
             logger.error("Gitlab callback error. Deploy ci not found, ciId: {}, callback params: {}", ciId, paramObj.toJSONString());
             throw new DeployCiNotFoundException(ciId);
         }
+        if (!Objects.equals(ci.getIsActive(), 1)) {
+            logger.info("Gitlab callback stop. Deploy ci is not active, ciId: {}, callback params: {}", ciId, paramObj.toJSONString());
+            return null;
+        }
+        String triggerType = ci.getTriggerType();
+        if (StringUtils.isBlank(triggerType)) {
+            logger.error("Gitlab callback error. Missing triggerTime in ci config, ciId: {}, callback params: {}", ciId, paramObj.toJSONString());
+            throw new DeployCiTriggerTypeLostException();
+        }
+        String triggerTimeStr = ci.getTriggerTime();
+        if (!DeployCiTriggerType.INSTANT.getValue().equals(ci.getTriggerType()) && StringUtils.isBlank(triggerTimeStr)) {
+            logger.error("Gitlab callback error. Missing triggerTime in ci config, ciId: {}, callback params: {}", ciId, paramObj.toJSONString());
+            throw new DeployCiTriggerTimeLostException();
+        }
         JSONObject versionRule = ci.getVersionRule();
         String versionPrefix = versionRule.getString("versionPrefix");
         String versionRegex = versionRule.getString("versionRegex");
         int useCommitId = versionRule.getInteger("useCommitId") != null ? versionRule.getInteger("useCommitId") : 0;
         String versionName = getVersionName(branchName, versionRegex, versionPrefix, commitId, useCommitId);
         DeployVersionVo deployVersion = deployVersionMapper.getDeployVersionBaseInfoBySystemIdAndModuleIdAndVersion(new DeployVersionVo(versionName, ci.getAppSystemId(), ci.getAppModuleId()));
-        /* todo
-            只要场景包含编译，那么新建buildNo和新建版本（如果版本不存在）
-            如果场景只包含部署，那么新建版本（如果版本不存在）
-            如果场景不含编译和部署，那么buildNo和版本都不新建
-         */
+        UserContext.init(SystemUser.SYSTEM.getUserVo(), SystemUser.SYSTEM.getTimezone());
+        UserContext.get().setToken("GZIP_" + LoginAuthHandlerBase.buildJwt(SystemUser.SYSTEM.getUserVo()).getCc());
         // 普通作业
         if (DeployCiActionType.CREATE_JOB.getValue().equals(ci.getAction())) {
+            /*
+              只要场景包含编译，那么新建buildNo和新建版本（如果版本不存在）
+              如果场景只包含部署，那么新建版本（如果版本不存在）
+              如果场景不含编译和部署，那么buildNo和版本都不新建
+            */
             Long scenarioId = ci.getConfig().getLong("scenarioId");
             if (scenarioId == null) {
                 logger.error("Gitlab callback error. Missing scenarioId in ci config, ciId: {}, callback params: {}", ciId, paramObj.toJSONString());
@@ -164,20 +199,7 @@ public class CallbackDeployCiGitlabEventApi extends PrivateApiComponentBase {
                 deployVersion = new DeployVersionVo(versionName, ci.getAppSystemId(), ci.getAppModuleId(), 0);
                 deployVersionMapper.insertDeployVersion(deployVersion);
             }
-            Date triggerTime = null;
-            if (StringUtils.isNotBlank(ci.getTriggerTime())) {
-                LocalTime triggerInstance = LocalTime.parse(ci.getTriggerTime(), DateTimeFormatter.ofPattern(TimeUtil.HH_MM_SS));
-                // 如果当前时间在前，则触发时间为当天；如果当前时间在后，则触发时间为第二日
-                String day;
-                if (LocalTime.now().isBefore(triggerInstance)) {
-                    day = LocalDate.now().format(DateTimeFormatter.ofPattern(TimeUtil.YYYY_MM_DD));
-                } else {
-                    day = LocalDate.now().plusDays(1L).format(DateTimeFormatter.ofPattern(TimeUtil.YYYY_MM_DD));
-                }
-                triggerTime = Date.from(LocalDateTime.parse(day + " " + ci.getTriggerTime()
-                        , DateTimeFormatter.ofPattern(TimeUtil.YYYY_MM_DD_HH_MM_SS)).atZone(ZoneId.systemDefault()).toInstant());
-            }
-            String triggerType = ci.getTriggerType();
+            Date triggerTime = getTriggerTime(triggerTimeStr);
             if (DeployCiTriggerType.INSTANT.getValue().equals(ci.getTriggerType())) {
                 triggerType = DeployCiTriggerType.AUTO.getValue();
             }
@@ -189,23 +211,22 @@ public class CallbackDeployCiGitlabEventApi extends PrivateApiComponentBase {
                 moduleVo.setBuildNo(-1);
             }
             deployJobParam.setModuleList(Collections.singletonList(moduleVo));
-            UserContext.init(SystemUser.SYSTEM.getUserVo(), SystemUser.SYSTEM.getTimezone());
-            UserContext.get().setToken("GZIP_" + LoginAuthHandlerBase.buildJwt(SystemUser.SYSTEM.getUserVo()).getCc());
             if (!Objects.equals(ci.getTriggerType(), DeployCiTriggerType.INSTANT.getValue())) {
                 deployJobService.createScheduleJob(deployJobParam, moduleVo);
             } else {
                 deployJobService.createJobAndFire(deployJobParam, moduleVo);
             }
         } else if (DeployCiActionType.CREATE_BATCH_JOB.getValue().equals(ci.getAction())) {
-            /* todo
-                1、遍历当前流水线所有属于当前模块的子作业，检查其场景以决定是否要创建版本和buildNo
-                2、筛选出属于当前模块的子作业来执行（执行的顺序？）
+            /*
+               1、遍历当前流水线所有属于当前模块的子作业，检查其场景以决定是否要创建版本
+               2、筛选出属于当前模块的子作业来执行
              */
             Long pipelineId = ci.getConfig().getLong("pipelineId");
             if (pipelineId == null) {
                 logger.error("Gitlab callback error. Missing pipelineId in ci config, ciId: {}, callback params: {}", ciId, paramObj.toJSONString());
                 throw new DeployCiPipelineIdLostException();
             }
+            // todo 获取所有环境？
             DeployPipelineConfigVo deployPipelineConfigVo = DeployPipelineConfigManager.init(ci.getAppSystemId())
                     .withAppModuleId(ci.getAppModuleId())
                     .isHasBuildOrDeployTypeTool(true)
@@ -243,11 +264,77 @@ public class CallbackDeployCiGitlabEventApi extends PrivateApiComponentBase {
                 deployVersion = new DeployVersionVo(versionName, ci.getAppSystemId(), ci.getAppModuleId(), 0);
                 deployVersionMapper.insertDeployVersion(deployVersion);
             }
+            String jobName = ci.getConfig().getString("jobName");
+            if (StringUtils.isBlank(jobName)) {
+                logger.error("Gitlab callback error. Missing jobName in ci config, ciId: {}, callback params: {}", ciId, paramObj.toJSONString());
+                throw new DeployCiJobNameLostException();
+            }
+            DeployJobVo deployJobVo = new DeployJobVo();
+            deployJobVo.setPipelineId(pipelineId);
+            deployJobVo.setName(jobName);
+            Date triggerTime = getTriggerTime(triggerTimeStr);
+            if (DeployCiTriggerType.MANUAL.getValue().equals(triggerType)) {
+                deployJobVo.setStatus(JobStatus.PENDING.getValue());
+                deployJobVo.setTriggerType(JobTriggerType.MANUAL.getValue());
+            } else {
+                deployJobVo.setStatus(JobStatus.READY.getValue());
+                deployJobVo.setTriggerType(JobTriggerType.AUTO.getValue());
+                deployJobVo.setPlanStartTime(triggerTime);
+            }
+            deployJobVo.setAppSystemModuleVersionList(Collections.singletonList(new DeploySystemModuleVersionVo(ci.getAppSystemId(), ci.getAppModuleId(), deployVersion.getId())));
+            deployJobVo.setReviewStatus(ReviewStatus.PASSED.getValue());
+            deployJobVo.setSource(JobSource.BATCHDEPLOY.getValue());
+            deployJobVo.setExecUser(UserContext.get().getUserUuid());
 
+            deployBatchJobService.creatBatchJob(deployJobVo, pipeline, false);
+            deployJobMapper.insertJobInvoke(deployJobVo.getId(), pipelineId, JobSource.PIPELINE.getValue());
+
+            //补充定时执行逻辑
+            if (Objects.equals(deployJobVo.getTriggerType(), JobTriggerType.AUTO.getValue())) {
+                IJob jobHandler = SchedulerManager.getHandler(DeployBatchJobAutoFireJob.class.getName());
+                if (jobHandler == null) {
+                    throw new ScheduleHandlerNotFoundException(DeployBatchJobAutoFireJob.class.getName());
+                }
+                JobObject.Builder jobObjectBuilder = new JobObject.Builder(deployJobVo.getId().toString(), jobHandler.getGroupName(), jobHandler.getClassName(), TenantContext.get().getTenantUuid());
+                jobHandler.reloadJob(jobObjectBuilder.build());
+            }
         }
         return null;
     }
 
+    /**
+     * 计算触发时间
+     *
+     * @param triggerTimeStr
+     * @return
+     */
+    private Date getTriggerTime(String triggerTimeStr) {
+        Date triggerTime = null;
+        if (StringUtils.isNotBlank(triggerTimeStr)) {
+            LocalTime triggerInstance = LocalTime.parse(triggerTimeStr, DateTimeFormatter.ofPattern(TimeUtil.HH_MM_SS));
+            // 如果当前时间在前，则触发时间为当天；如果当前时间在后，则触发时间为第二日
+            String day;
+            if (LocalTime.now().isBefore(triggerInstance)) {
+                day = LocalDate.now().format(DateTimeFormatter.ofPattern(TimeUtil.YYYY_MM_DD));
+            } else {
+                day = LocalDate.now().plusDays(1L).format(DateTimeFormatter.ofPattern(TimeUtil.YYYY_MM_DD));
+            }
+            triggerTime = Date.from(LocalDateTime.parse(day + " " + triggerTimeStr
+                    , DateTimeFormatter.ofPattern(TimeUtil.YYYY_MM_DD_HH_MM_SS)).atZone(ZoneId.systemDefault()).toInstant());
+        }
+        return triggerTime;
+    }
+
+    /**
+     * 计算版本号
+     *
+     * @param branchName    分支名
+     * @param versionRegex  分支名截取规则
+     * @param versionPrefix 版本前缀
+     * @param commitId      commitId
+     * @param useCommitId   是否拼接commitId
+     * @return
+     */
     private String getVersionName(String branchName, String versionRegex, String versionPrefix, String commitId, Integer useCommitId) {
         String versionName = StringUtils.EMPTY;
         String regex = StringUtils.EMPTY;
@@ -269,9 +356,9 @@ public class CallbackDeployCiGitlabEventApi extends PrivateApiComponentBase {
             Pattern r = Pattern.compile(regex);
             Matcher m = r.matcher(branchName);
             if (m.find()) {
-                versionName += m.group() + "_";
+                versionName += m.group();
             } else {
-                versionName += branchName + "_";
+                versionName += branchName;
             }
         }
         if (StringUtils.isNotBlank(versionPrefix)) {
@@ -279,9 +366,9 @@ public class CallbackDeployCiGitlabEventApi extends PrivateApiComponentBase {
         }
         if (StringUtils.isNotBlank(commitId) && Objects.equals(useCommitId, 1)) {
             if (commitId.length() > 8) {
-                versionName += commitId.substring(0, 8);
+                versionName += ("_" + commitId.substring(0, 8));
             } else {
-                versionName += commitId;
+                versionName += ("_" + commitId);
             }
         }
         return versionName;
