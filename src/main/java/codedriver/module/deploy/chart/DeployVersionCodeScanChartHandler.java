04f942f925e9ce6c1774ab5eb3826ff118b29dc9
赖文韬 <laiwt@techsure.com.cn>
2022-10-11 15:24:55 +0800

    --story=1006924 --user=赖文韬 自动发布-版本中心-新增代码扫描tab https://www.tapd.cn/54247054/s/1255829

--- src/main/java/codedriver/module/deploy/chart/DeployVersionCodeScanChartHandler.java
+++ src/main/java/codedriver/module/deploy/chart/DeployVersionCodeScanChartHandler.java
@@ -2,12 +2,162 @@ package codedriver.module.deploy.chart;
 
 import codedriver.framework.deploy.chart.DeployVersionChartHandlerBase;
 import codedriver.framework.deploy.constvalue.DeployVersionChartMenu;
+import codedriver.framework.deploy.dto.version.DeployVersionBuildQualityVo;
+import codedriver.module.deploy.dao.mapper.DeployVersionMapper;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class DeployVersionCodeScanChartHandler extends DeployVersionChartHandlerBase {
 
+    @Resource
+    DeployVersionMapper deployVersionMapper;
+
+    private final static Map<String, Function<Long, Object>> chartMap = new HashMap<>();
+
+    final static String LAST_CODE_ISSUE = "last_code_issue"; //最近一次代码问题
+    final static String LAST_FIVE_ANNOTATION_RATE = "last_five_annotation_rate"; //最近五次注释率
+    final static String LAST_FIVE_CODE_STATISTICS = "last_five_code_statistics"; //最近五次代码统计
+    final static String CODE_QUALITY_TREND = "code_quality_trend"; //bug/漏洞/代码异味趋势图
+    final static String CODE_REPEATABILITY = "code_repeatability"; //重复度趋势图
+
+    @PostConstruct
+    public void init() {
+
+        chartMap.put(LAST_CODE_ISSUE, (versionId) -> {
+            JSONObject data = new JSONObject();
+            JSONArray dataList = new JSONArray();
+            data.put("data", dataList);
+            List<DeployVersionBuildQualityVo> list = deployVersionMapper.getDeployVersionBuildQualityListByVersionIdWithLimit(versionId, 1);
+            if (list.size() > 0) {
+
+                DeployVersionBuildQualityVo qualityVo = list.get(0);
+                dataList.add(new JSONObject() {
+                    {
+                        this.put("value", qualityVo.getBlockerViolations());
+                        this.put("name", "blocker issues");
+                    }
+                });
+                dataList.add(new JSONObject() {
+                    {
+                        this.put("value", qualityVo.getCriticalViolations());
+                        this.put("name", "critical issues");
+                    }
+                });
+            }
+            return data;
+        });
+
+        chartMap.put(LAST_FIVE_ANNOTATION_RATE, (versionId) -> {
+            JSONObject data = new JSONObject();
+            data.put("legend", new JSONObject() {
+                {
+                    this.put("commentLinesDensity", "行注释率");
+                    this.put("publicDocumentedApiDensity", "API注释率");
+                }
+            });
+            JSONArray dataList = new JSONArray();
+            data.put("data", dataList);
+            List<DeployVersionBuildQualityVo> list = deployVersionMapper.getDeployVersionBuildQualityListByVersionIdWithLimit(versionId, 5);
+            if (list.size() > 0) {
+                for (DeployVersionBuildQualityVo vo : list) {
+                    dataList.add(new JSONObject() {
+                        {
+                            this.put("buildTime", vo.getBuildTime());
+                            this.put("commentLinesDensity", vo.getCommentLinesDensity());
+                            this.put("publicDocumentedApiDensity", vo.getPublicDocumentedApiDensity());
+                        }
+                    });
+                }
+            }
+            return data;
+        });
+
+        chartMap.put(LAST_FIVE_CODE_STATISTICS, (versionId) -> {
+            JSONObject data = new JSONObject();
+            data.put("legend", new JSONObject() {
+                {
+                    this.put("lines", "代码总行数");
+                    this.put("ncloc", "代码有效行");
+                }
+            });
+            JSONArray dataList = new JSONArray();
+            data.put("data", dataList);
+            List<DeployVersionBuildQualityVo> list = deployVersionMapper.getDeployVersionBuildQualityListByVersionIdWithLimit(versionId, 5);
+            if (list.size() > 0) {
+                for (DeployVersionBuildQualityVo vo : list) {
+                    dataList.add(new JSONObject() {
+                        {
+                            this.put("buildTime", vo.getBuildTime());
+                            this.put("lines", vo.getLines());
+                            this.put("ncloc", vo.getNcloc());
+                        }
+                    });
+                }
+            }
+            return data;
+        });
+
+        chartMap.put(CODE_QUALITY_TREND, (versionId) -> {
+            JSONObject data = new JSONObject();
+            data.put("legend", new JSONObject() {
+                {
+                    this.put("bugs", "Bugs");
+                    this.put("vulnerabilities", "漏洞");
+                    this.put("codeSmells", "代码异味");
+                }
+            });
+            JSONArray dataList = new JSONArray();
+            data.put("data", dataList);
+            List<DeployVersionBuildQualityVo> list = deployVersionMapper.getDeployVersionBuildQualityListByVersionIdWithLimit(versionId, 5);
+            if (list.size() > 0) {
+                for (DeployVersionBuildQualityVo vo : list) {
+                    dataList.add(new JSONObject() {
+                        {
+                            this.put("buildTime", vo.getBuildTime());
+                            this.put("bugs", vo.getBugs());
+                            this.put("vulnerabilities", vo.getVulnerabilities());
+                            this.put("codeSmells", vo.getCodeSmells());
+                        }
+                    });
+                }
+            }
+            return data;
+        });
+
+        chartMap.put(CODE_REPEATABILITY, (versionId) -> {
+            JSONObject data = new JSONObject();
+            data.put("legend", new JSONObject() {
+                {
+                    this.put("duplicatedLinesDensity", "重复度");
+                }
+            });
+            JSONArray dataList = new JSONArray();
+            data.put("data", dataList);
+            List<DeployVersionBuildQualityVo> list = deployVersionMapper.getDeployVersionBuildQualityListByVersionIdWithLimit(versionId, 5);
+            if (list.size() > 0) {
+                for (DeployVersionBuildQualityVo vo : list) {
+                    dataList.add(new JSONObject() {
+                        {
+                            this.put("buildTime", vo.getBuildTime());
+                            this.put("duplicatedLinesDensity", vo.getDuplicatedLinesDensity());
+                        }
+                    });
+                }
+            }
+            return data;
+        });
+    }
+
     @Override
     protected Object myGetChartData(String chartType, Long versionId) {
-        return null;
+        return chartMap.get(chartType).apply(versionId);
     }
 
     @Override
--- src/main/java/codedriver/module/deploy/chart/DeployVersionUnitTestChartHandler.java
+++ src/main/java/codedriver/module/deploy/chart/DeployVersionUnitTestChartHandler.java
@@ -2,12 +2,109 @@ package codedriver.module.deploy.chart;
 
 import codedriver.framework.deploy.chart.DeployVersionChartHandlerBase;
 import codedriver.framework.deploy.constvalue.DeployVersionChartMenu;
+import codedriver.framework.deploy.dto.version.DeployVersionUnitTestVo;
+import codedriver.module.deploy.dao.mapper.DeployVersionMapper;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class DeployVersionUnitTestChartHandler extends DeployVersionChartHandlerBase {
 
+    @Resource
+    DeployVersionMapper deployVersionMapper;
+
+    private final static Map<String, Function<Long, Object>> chartMap = new HashMap<>();
+
+    final static String LAST_CODE_TEST_RESULT = "LAST_CODE_TEST_RESULT"; //最近一次代码测试结果
+    final static String LAST_FIVE_INCREMENTAL_COVERAGE_RATE = "last_five_incremental_coverage_rate"; //最近五次增量覆盖率
+    final static String LAST_FIVE_FULL_COVERAGE_RATE = "last_five_full_coverage_rate"; //最近五次全量覆盖率
+
+    @PostConstruct
+    public void init() {
+
+        chartMap.put(LAST_CODE_TEST_RESULT, (versionId) -> {
+            JSONObject data = new JSONObject();
+            JSONArray dataList = new JSONArray();
+            data.put("data", dataList);
+            List<DeployVersionUnitTestVo> list = deployVersionMapper.getDeployVersionUnitTestListByVersionIdWithLimit(versionId, 1);
+            if (list.size() > 0) {
+                DeployVersionUnitTestVo unitTestVo = list.get(0);
+                dataList.add(new JSONObject() {
+                    {
+                        this.put("value", Math.round(unitTestVo.getTests() * unitTestVo.getTestSuccessDensity() / 100));
+                        this.put("name", "成功");
+                    }
+                });
+                dataList.add(new JSONObject() {
+                    {
+                        this.put("value", unitTestVo.getTestErrors());
+                        this.put("name", "失败");
+                    }
+                });
+            }
+            return data;
+        });
+
+        chartMap.put(LAST_FIVE_INCREMENTAL_COVERAGE_RATE, (versionId) -> {
+            JSONObject data = new JSONObject();
+            data.put("legend", new JSONObject() {
+                {
+                    this.put("newBranchCoverage", "分支");
+                    this.put("newLineCoverage", "行");
+                }
+            });
+            JSONArray dataList = new JSONArray();
+            data.put("data", dataList);
+            List<DeployVersionUnitTestVo> list = deployVersionMapper.getDeployVersionUnitTestListByVersionIdWithLimit(versionId, 1);
+            if (list.size() > 0) {
+                for (DeployVersionUnitTestVo vo : list) {
+                    dataList.add(new JSONObject() {
+                        {
+                            this.put("buildTime", vo.getBuildTime());
+                            this.put("newBranchCoverage", vo.getNewBranchCoverage());
+                            this.put("newLineCoverage", vo.getNewLineCoverage());
+                        }
+                    });
+                }
+            }
+            return data;
+        });
+
+        chartMap.put(LAST_FIVE_FULL_COVERAGE_RATE, (versionId) -> {
+            JSONObject data = new JSONObject();
+            data.put("legend", new JSONObject() {
+                {
+                    this.put("branchCoverage", "分支");
+                    this.put("lineCoverage", "行");
+                }
+            });
+            JSONArray dataList = new JSONArray();
+            data.put("data", dataList);
+            List<DeployVersionUnitTestVo> list = deployVersionMapper.getDeployVersionUnitTestListByVersionIdWithLimit(versionId, 1);
+            if (list.size() > 0) {
+                for (DeployVersionUnitTestVo vo : list) {
+                    dataList.add(new JSONObject() {
+                        {
+                            this.put("buildTime", vo.getBuildTime());
+                            this.put("branchCoverage", vo.getBranchCoverage());
+                            this.put("lineCoverage", vo.getLineCoverage());
+                        }
+                    });
+                }
+            }
+            return data;
+        });
+    }
+
     @Override
     protected Object myGetChartData(String chartType, Long versionId) {
-        return null;
+        return chartMap.get(chartType).apply(versionId);
     }
 
     @Override
