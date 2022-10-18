f36fde25904081ae054cab8588ee171f8440fe2c
longrf <a5739119>
2022-10-18 15:26:05 +0800

    --story=1007022 --user=龙儒锋 后端-自动化-场景删除判断提示修改 https://www.tapd.cn/54247054/s/1257123

--- src/main/java/codedriver/module/deploy/dependency/handler/AutoexecScenarioDeployPipelineDependencyHandler.java
+++ src/main/java/codedriver/module/deploy/dependency/handler/AutoexecScenarioDeployPipelineDependencyHandler.java
@@ -14,13 +14,11 @@ import codedriver.framework.dependency.core.IFromType;
 import codedriver.framework.dependency.dto.DependencyInfoVo;
 import codedriver.framework.dependency.dto.DependencyVo;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * @author longrf
@@ -31,8 +29,7 @@ import java.util.Objects;
 public class AutoexecScenarioDeployPipelineDependencyHandler extends FixedTableDependencyHandlerBase {
     @Override
     protected DependencyInfoVo parse(DependencyVo dependencyVo) {
-
-        //由于删除配置项不会删除其关联的引用关系，因此查询时需要判断配置项是否还存在
+        /*暂时前端没有需要依赖跳转，此方法暂时不会被调用*/
 
         JSONObject config = dependencyVo.getConfig();
         if (MapUtils.isEmpty(config)) {
@@ -43,24 +40,6 @@ public class AutoexecScenarioDeployPipelineDependencyHandler extends FixedTableD
         Long appSystemId = config.getLong("appSystemId");
         Long moduleId = config.getLong("moduleId") != 0L ? config.getLong("moduleId") : null;
         Long envId = config.getLong("envId") != 0L ? config.getLong("envId") : null;
-        List<Long> ciEntityIdList = new ArrayList<>();
-        ciEntityIdList.add(appSystemId);
-        if (Objects.nonNull(moduleId)) {
-            ciEntityIdList.add(moduleId);
-        }
-        if (Objects.nonNull(envId)) {
-            ciEntityIdList.add(envId);
-        }
-
-        ICiEntityCrossoverMapper ciEntityCrossoverMapper = CrossoverServiceFactory.getApi(ICiEntityCrossoverMapper.class);
-
-        //判断配置项是否仍存在
-        List<CiEntityVo> ciEntityVoList = ciEntityCrossoverMapper.getCiEntityBaseInfoByIdList(ciEntityIdList);
-        if (CollectionUtils.isEmpty(ciEntityVoList) || ciEntityVoList.size() != ciEntityIdList.size()) {
-            //如果系统、模块、环境被删除，而这层关系也需要删除
-            delete(configId);
-            return null;
-        }
 
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("/");
@@ -78,6 +57,7 @@ public class AutoexecScenarioDeployPipelineDependencyHandler extends FixedTableD
         }
         List<String> pathList = new ArrayList<>();
         pathList.add("应用配置");
+        ICiEntityCrossoverMapper ciEntityCrossoverMapper = CrossoverServiceFactory.getApi(ICiEntityCrossoverMapper.class);
         if (appSystemId != null && appSystemId != 0) {
             CiEntityVo ciEntityVo = ciEntityCrossoverMapper.getCiEntityBaseInfoById(appSystemId);
             if (ciEntityVo != null) {
