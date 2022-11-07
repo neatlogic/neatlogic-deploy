375c90321414a8a9fd2baf825157bef662cbf9b7
赖文韬 <laiwt@techsure.com.cn>
2022-11-07 19:02:17 +0800

    --story=1007113 --user=赖文韬 批量更新gitlab webhook接口 https://www.tapd.cn/54247054/s/1260199

--- src/main/java/codedriver/module/deploy/api/ci/BatchUpdateGitlabHookApi.java
+++ src/main/java/codedriver/module/deploy/api/ci/BatchUpdateGitlabHookApi.java
@@ -17,6 +17,7 @@ import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.framework.util.HttpRequestUtil;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
@@ -43,11 +44,15 @@ public class BatchUpdateGitlabHookApi extends PrivateApiComponentBase {
 
     @Input({
             @Param(name = "runnerUrl", desc = "runner url", type = ApiParamType.STRING, isRequired = true),
+            @Param(name = "repoServerAddress", desc = "仓库服务器地址", type = ApiParamType.STRING, isRequired = true),
+            @Param(name = "branchFilter", desc = "分支", type = ApiParamType.STRING),
+            @Param(name = "username", desc = "gitlab用户", type = ApiParamType.STRING),
+            @Param(name = "password", desc = "gitlab密码", type = ApiParamType.STRING),
             @Param(name = "configList", desc = "hook配置列表", type = ApiParamType.JSONARRAY, isRequired = true),
             @Param(name = "configList.repoServerAddress", desc = "仓库服务器地址"),
             @Param(name = "configList.repoName", desc = "仓库名称"),
             @Param(name = "configList.branchFilter", desc = "分支", type = ApiParamType.STRING),
-            @Param(name = "configList.callbackUrl", desc = "回调url", type = ApiParamType.STRING),
+            @Param(name = "configList.callbackUrl", desc = "回调url", type = ApiParamType.JSONARRAY),
             @Param(name = "configList.username", desc = "gitlab用户", type = ApiParamType.STRING),
             @Param(name = "configList.password", desc = "gitlab密码", type = ApiParamType.STRING),
             @Param(name = "configList.action", desc = "动作类型", type = ApiParamType.ENUM, rule = "insert,delete"),
@@ -56,48 +61,63 @@ public class BatchUpdateGitlabHookApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         JSONArray array = new JSONArray();
+        String globalRepoServerAddress = paramObj.getString("repoServerAddress");
+        String globalBranchFilter = paramObj.getString("branchFilter");
+        String globalUsername = paramObj.getString("username");
+        String globalPassword = paramObj.getString("password");
+        String encPassword = null;
+        if (StringUtils.isNotBlank(globalPassword)) {
+            encPassword = RC4Util.encrypt(globalPassword);
+        }
         JSONArray configList = paramObj.getJSONArray("configList");
         for (int i = 0; i < configList.size(); i++) {
             JSONObject config = configList.getJSONObject(i);
-            String callbackUrl = config.getString("callbackUrl");
-            String repoServerAddress = config.getString("repoServerAddress");
+            JSONArray callbackUrl = config.getJSONArray("callbackUrl");
             String repoName = config.getString("repoName");
             String username = config.getString("username");
             String password = config.getString("password");
             String action = config.getString("action");
-            if (StringUtils.isBlank(repoServerAddress)) {
-                throw new ApiRuntimeException("第：" + (i + 1) + "个缺少repoServerAddress");
-            }
             if (StringUtils.isBlank(repoName)) {
-                throw new ApiRuntimeException(repoServerAddress + "缺少repoName");
+                throw new ApiRuntimeException("第：" + (i + 1) + "个缺少repoName");
+            }
+            if (CollectionUtils.isEmpty(callbackUrl)) {
+                throw new ApiRuntimeException(repoName + "缺少callbackUrl");
             }
             if (StringUtils.isBlank(username)) {
-                throw new ApiRuntimeException(repoServerAddress + "/" + repoName + "缺少username");
+                config.put("username", globalUsername);
+                if (StringUtils.isBlank(globalUsername)) {
+                    throw new ApiRuntimeException(repoName + "缺少username");
+                }
             }
             if (StringUtils.isBlank(password)) {
-                throw new ApiRuntimeException(repoServerAddress + "/" + repoName + "缺少password");
+                config.put("password", encPassword);
+                if (StringUtils.isBlank(encPassword)) {
+                    throw new ApiRuntimeException(repoName + "缺少password");
+                }
+            } else {
+                config.put("password", RC4Util.encrypt(password));
+            }
+            if (StringUtils.isBlank(config.getString("branchFilter"))) {
+                config.put("branchFilter", globalBranchFilter);
             }
             if (StringUtils.isBlank(action)) {
-                throw new ApiRuntimeException(repoServerAddress + "/" + repoName + "缺少action");
+                throw new ApiRuntimeException(repoName + "缺少action");
             }
             if (!"insert".equals(action) && !"delete".equals(action)) {
-                throw new ApiRuntimeException(repoServerAddress + "/" + repoName + "未知的action：" + action);
+                throw new ApiRuntimeException(repoName + "未知的action：" + action);
             }
-            if ("insert".equals(action) && StringUtils.isBlank(callbackUrl)) {
-                throw new ApiRuntimeException(repoServerAddress + "/" + repoName + "缺少callbackUrl");
-            }
+            config.put("repoServerAddress", globalRepoServerAddress);
             config.put("authMode", DeployCiGitlabAuthMode.ACCESS_TOKEN.getValue());
             config.put("event", DeployCiRepoEvent.POSTRECEIVE.getValue());
-            config.put(password, RC4Util.encrypt(password));
             array.add(config);
         }
-        String url = paramObj.getString("runnerUrl") + "/api/rest/deploy/ci/gitlabwebhook/batchupdate";
+        String url = paramObj.getString("runnerUrl") + "/autoexecrunner/api/rest/deploy/ci/gitlabwebhook/batchupdate";
         JSONObject param = new JSONObject();
         param.put("configList", array);
         HttpRequestUtil request = HttpRequestUtil.post(url).setPayload(param.toJSONString()).setAuthType(AuthenticateType.BUILDIN).sendRequest();
         JSONObject resultJson = request.getResultJson();
         if (MapUtils.isNotEmpty(resultJson)) {
-            return resultJson.getJSONArray("Message");
+            return resultJson.getJSONArray("Return");
         }
         return null;
     }
