485fb4bfbc62e042d44caf08aaaaec59f2b0e8a2
lvzk <897706680@qq.com>
2023-05-15 19:50:24 +0800

    --story=1007660 --user=lvzk 后端startup优化,额外支持dml.sql的方式只执行一次维护初始化出厂数据 https://www.tapd.cn/54247054/s/1273336

---
+++ src/main/resources/neatlogic/resources/deploy/sqlscript/dml.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (5, 1);
+INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (6, 1);
+INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (7, 1);
+INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (9, 1);
+INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (11, 1);
+INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (13, 1);
