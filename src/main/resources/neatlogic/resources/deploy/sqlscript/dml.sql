b6760faffecd883b759782f8764664e0c6ae1b6d
lvzk <897706680@qq.com>
2023-07-14 17:44:57 +0800

    [功能]注册租户初始化数据

--- src/main/resources/neatlogic/resources/deploy/sqlscript/dml.sql
+++ src/main/resources/neatlogic/resources/deploy/sqlscript/dml.sql
@@ -1,6 +1,12 @@
-INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (5, 1);
-INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (6, 1);
-INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (7, 1);
-INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (9, 1);
-INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (11, 1);
-INSERT IGNORE INTO `deploy_type_status` (`type_id`, `is_active`) VALUES (13, 1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (2,0);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (5,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (6,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (7,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (9,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (11,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (13,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (477316643823616,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (477317977612288,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (477319286235136,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (651160662843392,1);
+insert  into `deploy_type_status`(`type_id`,`is_active`) values (681687327449088,1);
