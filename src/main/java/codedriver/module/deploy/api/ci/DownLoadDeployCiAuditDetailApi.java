900a705ce5fe0d1ab8e50fca59b776e98cb6346d
linbangquan <1437892690@qq.com>
2023-07-27 19:51:59 +0800

    [修复] 漏洞修复
    关联 #[943241577332736]漏洞修复 http://192.168.0.96:8090/demo/rdm.html#/task-list/doing

--- src/main/java/codedriver/module/deploy/api/ci/DownLoadDeployCiAuditDetailApi.java
+++ src/main/java/codedriver/module/deploy/api/ci/DownLoadDeployCiAuditDetailApi.java
@@ -11,17 +11,15 @@ import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.crossover.CrossoverServiceFactory;
 import codedriver.framework.crossover.IFileCrossoverService;
 import codedriver.framework.deploy.auth.DEPLOY_BASE;
+import codedriver.framework.file.dto.AuditFilePathVo;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateBinaryStreamApiComponentBase;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 @Service
@@ -51,39 +49,13 @@ public class DownLoadDeployCiAuditDetailApi extends PrivateBinaryStreamApiCompon
 	@Description(desc = "下载持续集成回调接口调用日志")
 	@Override
 	public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		Map<String, String> paramMap = new HashMap<>();
 		String filePath = paramObj.getString("filePath");
-		String[] split = filePath.split("\\?");
-		String path = split[0];
-		if (split.length >= 2) {
-			String[] paramArray = split[1].split("&");
-			for (String param : paramArray) {
-				if (param.contains("=")) {
-					String[] paramKeyValue = param.split("=");
-					paramMap.put(paramKeyValue[0], paramKeyValue[1]);
-				}
-			}
-		}
-		int startIndex = 0;
-		int offset = 0;
-		int serverId = 0;
-		String startIndexStr = paramMap.get("startIndex");
-		if (StringUtils.isNotBlank(startIndexStr)) {
-			startIndex = Integer.parseInt(startIndexStr);
-		}
-		String offsetStr = paramMap.get("offset");
-		if (StringUtils.isNotBlank(offsetStr)) {
-			offset = Integer.parseInt(offsetStr);
-		}
-		String serverIdStr = paramMap.get("serverId");
-		if (StringUtils.isNotBlank(serverIdStr)) {
-			serverId = Integer.parseInt(serverIdStr);
-		}
+		AuditFilePathVo auditFilePathVo = new AuditFilePathVo(filePath);
 		IFileCrossoverService fileCrossoverService = CrossoverServiceFactory.getApi(IFileCrossoverService.class);
-		if (Objects.equals(serverId, Config.SCHEDULE_SERVER_ID)) {
-			fileCrossoverService.downloadLocalFile(path, startIndex, offset, response);
+		if (Objects.equals(auditFilePathVo.getServerId(), Config.SCHEDULE_SERVER_ID)) {
+			fileCrossoverService.downloadLocalFile(auditFilePathVo.getPath(), auditFilePathVo.getStartIndex(), auditFilePathVo.getOffset(), response);
 		} else {
-			fileCrossoverService.downloadRemoteFile(paramObj, serverId, request, response);
+			fileCrossoverService.downloadRemoteFile(paramObj, auditFilePathVo.getServerId(), request, response);
 		}
 		return null;
 	}
--- src/main/java/codedriver/module/deploy/api/ci/GetDeployCiAuditDetailApi.java
+++ src/main/java/codedriver/module/deploy/api/ci/GetDeployCiAuditDetailApi.java
@@ -11,6 +11,7 @@ import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.crossover.CrossoverServiceFactory;
 import codedriver.framework.crossover.IFileCrossoverService;
 import codedriver.framework.deploy.auth.DEPLOY_BASE;
+import codedriver.framework.file.dto.AuditFilePathVo;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
@@ -52,40 +53,13 @@ public class GetDeployCiAuditDetailApi extends PrivateApiComponentBase {
     @Description(desc = "获取持续集成回调接口调用日志")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-
-        Map<String, String> paramMap = new HashMap<>();
         String filePath = paramObj.getString("filePath");
-        String[] split = filePath.split("\\?");
-        String path = split[0];
-        if (split.length >= 2) {
-            String[] paramArray = split[1].split("&");
-            for (String param : paramArray) {
-                if (param.contains("=")) {
-                    String[] paramKeyValue = param.split("=");
-                    paramMap.put(paramKeyValue[0], paramKeyValue[1]);
-                }
-            }
-        }
-        int startIndex = 0;
-        int offset = 0;
-        int serverId = 0;
-        String startIndexStr = paramMap.get("startIndex");
-        if (StringUtils.isNotBlank(startIndexStr)) {
-            startIndex = Integer.parseInt(startIndexStr);
-        }
-        String offsetStr = paramMap.get("offset");
-        if (StringUtils.isNotBlank(offsetStr)) {
-            offset = Integer.parseInt(offsetStr);
-        }
-        String serverIdStr = paramMap.get("serverId");
-        if (StringUtils.isNotBlank(serverIdStr)) {
-            serverId = Integer.parseInt(serverIdStr);
-        }
+        AuditFilePathVo auditFilePathVo = new AuditFilePathVo(filePath);
         IFileCrossoverService fileCrossoverService = CrossoverServiceFactory.getApi(IFileCrossoverService.class);
-        if (Objects.equals(serverId, Config.SCHEDULE_SERVER_ID)) {
-            return fileCrossoverService.readLocalFile(path, startIndex, offset);
+        if (Objects.equals(auditFilePathVo.getServerId(), Config.SCHEDULE_SERVER_ID)) {
+            return fileCrossoverService.readLocalFile(auditFilePathVo.getPath(), auditFilePathVo.getStartIndex(), auditFilePathVo.getOffset());
         } else {
-            return fileCrossoverService.readRemoteFile(paramObj, serverId);
+            return fileCrossoverService.readRemoteFile(paramObj, auditFilePathVo.getServerId());
         }
     }
 }
