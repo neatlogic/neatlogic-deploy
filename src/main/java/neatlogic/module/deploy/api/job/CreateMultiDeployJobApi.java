216d299ffad92ec9a72e24fcb632c86a49405284
lvzk <897706680@qq.com>
2025-03-18 19:58:20 +0800

    [功能]优化自动化、发布requestUtil异常提示 [功能]发布废弃getDeployVersionAppbuildCredentialByProxyToUrl，改为系统虚拟用户

--- src/main/java/neatlogic/module/deploy/api/job/CreateMultiDeployJobApi.java
+++ src/main/java/neatlogic/module/deploy/api/job/CreateMultiDeployJobApi.java
@@ -168,7 +168,7 @@ public class CreateMultiDeployJobApi extends PrivateApiComponentBase {
         //改为系统虚拟用户
         HttpServletRequest request = UserContext.get().getRequest();
         HttpServletResponse response = UserContext.get().getResponse();
-        UserContext.init(SystemUser.AUTOEXEC);
+        UserContext.init(SystemUser.AUTOEXEC, request, response);
         UserContext.get().setToken("GZIP_" + LoginAuthHandlerBase.buildJwt(SystemUser.AUTOEXEC.getUserVo()).getCc());
         String requestURI = request.getRequestURI();
         String url = proxyToUrl + requestURI;
