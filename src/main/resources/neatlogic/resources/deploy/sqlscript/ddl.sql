d14da875ecad3d51587257c61d7dd582171cc2f1
1437892690@qq.com <1437892690@qq.com>
2025-04-15 18:59:38 +0800

    [功能] 应用配置-环境层配置文件适配变量增加设为空标志位
    关联 #[1399994194558976]应用配置-环境层配置文件适配变量增加设为空标志位 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1399994194558976

--- src/main/resources/neatlogic/resources/deploy/sqlscript/ddl.sql
+++ src/main/resources/neatlogic/resources/deploy/sqlscript/ddl.sql
@@ -94,18 +94,19 @@ CREATE TABLE `deploy_app_config_user`  (
 -- ----------------------------
 -- Table structure for deploy_app_env_auto_config
 -- ----------------------------
-CREATE TABLE `deploy_app_env_auto_config`  (
+CREATE TABLE `deploy_app_env_auto_config` (
   `app_system_id` bigint NOT NULL COMMENT '应用id',
   `app_module_id` bigint NOT NULL COMMENT '模块id',
   `env_id` bigint NOT NULL COMMENT '环境资产id',
   `instance_id` bigint NOT NULL COMMENT '实例资产id',
   `key` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '变量名',
-  `type` enum('text','password') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'text' COMMENT '变量类型',
-  `value` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '变量值',
+  `type` enum('text','password') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'text' COMMENT '变量类型',
+  `value` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '变量值',
+  `is_empty` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否设为空',
   `lcd` timestamp(3) NULL DEFAULT NULL COMMENT '最后一次修改时间',
-  `lcu` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '最后一次修改人',
-  PRIMARY KEY (`app_system_id`, `app_module_id`, `env_id`, `instance_id`, `key`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '发布应用环境变量配置';
+  `lcu` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '最后一次修改人',
+  PRIMARY KEY (`app_system_id`,`app_module_id`,`env_id`,`instance_id`,`key`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='发布应用环境变量配置';
 
 -- ----------------------------
 -- Table structure for deploy_app_module_runner_group
