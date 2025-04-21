791b293ed970ae7460e253e7596c3fcadb000a31
lvzk <897706680@qq.com>
2025-04-21 16:33:07 +0800

    [功能]发布支持蓝绿执行策略 #[1402157146800128]发布支持蓝绿执行策略 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1402157146800128

--- src/main/resources/neatlogic/resources/deploy/changelog/2025-04-19/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/deploy/changelog/2025-04-19/neatlogic_tenant.sql
@@ -1,25 +1,29 @@
 CREATE TABLE IF NOT EXISTS `deploy_blue_green`  (
   `id` bigint NOT NULL COMMENT 'id',
-  `name` varchar(50) NULL COMMENT '姓名',
-  `sort` integer NULL COMMENT '排序',
-  `is_active` tinyint(1) NULL DEFAULT 1 COMMENT '是否激活',
+  `name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '姓名',
+  `sort` int DEFAULT NULL COMMENT '排序',
+  `is_active` tinyint(1) DEFAULT '1' COMMENT '是否激活',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `uniqu`(`id`)
-) COMMENT = '发布蓝绿表';
+  UNIQUE KEY `uniqu` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='发布蓝绿表';
 
 CREATE TABLE IF NOT EXISTS `deploy_app_instance_blue_green`  (
-  `id` bigint NOT NULL COMMENT 'id',
-  `app_system_id` bigint NULL COMMENT '系统id',
-  `app_module_id` bigint NULL COMMENT '模块id',
-  `env_id` bigint NULL COMMENT '环境id',
-  `resource_id` bigint NULL COMMENT '实例id',
-  `blue_green_id` bigint NULL COMMENT '蓝绿id',
-   PRIMARY KEY (`id`, `app_system_id`, `app_module_id`, `env_id`, `resource_id`) USING BTREE
-) COMMENT = '发布实例蓝绿关系表';
+  `app_system_id` bigint NOT NULL COMMENT '系统id',
+  `app_module_id` bigint NOT NULL COMMENT '模块id',
+  `env_id` bigint NOT NULL COMMENT '环境id',
+  `resource_id` bigint NOT NULL COMMENT '实例id',
+  `blue_green_id` bigint DEFAULT NULL COMMENT '蓝绿id',
+  PRIMARY KEY (`app_system_id`,`app_module_id`,`env_id`,`resource_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='发布实例蓝绿关系表';
+
 
 
 CREATE TABLE IF NOT EXISTS `deploy_job_phase_node_blue_green`  (
+  `job_id` bigint DEFAULT NULL COMMENT '作业id',
+  `job_phase_id` bigint NOT NULL COMMENT '作业阶段id',
   `job_phase_node_id` bigint NOT NULL COMMENT '作业节点ID',
   `blue_green_id` bigint NOT NULL COMMENT '蓝绿ID',
-  PRIMARY KEY (`job_phase_node_id`, `blue_green_id`)
-) COMMENT = '发布作业蓝绿id';
+  `update_tag` bigint DEFAULT NULL COMMENT '跟新标记',
+  PRIMARY KEY (`job_phase_node_id`,`blue_green_id`),
+  KEY `idx_phase_id` (`job_phase_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='发布作业蓝绿id';
