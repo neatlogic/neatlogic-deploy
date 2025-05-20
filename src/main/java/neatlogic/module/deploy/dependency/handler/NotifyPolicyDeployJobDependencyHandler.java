0b1cbc0fb574cd6d0111981acfddbbf597bb299a
linbangquan <1437892690@qq.com>
2025-05-20 08:26:23 +0000

    !72 [修复] 发布作业默认通知策略没有生效 * [修复] 发布作业默认通知策略没有生效

--- src/main/java/neatlogic/module/deploy/dependency/handler/NotifyPolicyDeployJobDependencyHandler.java
+++ src/main/java/neatlogic/module/deploy/dependency/handler/NotifyPolicyDeployJobDependencyHandler.java
@@ -20,75 +20,88 @@ import neatlogic.framework.cmdb.crossover.IAppSystemMapper;
 import neatlogic.framework.cmdb.dto.resourcecenter.entity.AppSystemVo;
 import neatlogic.framework.crossover.CrossoverServiceFactory;
 import neatlogic.framework.dependency.constvalue.FrameworkFromType;
-import neatlogic.framework.dependency.core.CustomDependencyHandlerBase;
+import neatlogic.framework.dependency.core.DefaultDependencyHandlerBase;
 import neatlogic.framework.dependency.core.IFromType;
 import neatlogic.framework.dependency.dto.DependencyInfoVo;
-import neatlogic.framework.deploy.dto.app.DeployAppConfigVo;
-import neatlogic.module.deploy.dao.mapper.DeployAppConfigMapper;
+import neatlogic.framework.dependency.dto.DependencyVo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author longrf
  * @date 2022/12/29 16:36
  */
 @Service
-public class NotifyPolicyDeployJobDependencyHandler extends CustomDependencyHandlerBase {
+public class NotifyPolicyDeployJobDependencyHandler extends DefaultDependencyHandlerBase {
 
-    @Resource
-    DeployAppConfigMapper deployAppConfigMapper;
-
-    @Override
-    protected String getTableName() {
-        return "deploy_job_notify_policy";
-    }
-
-    @Override
-    protected String getFromField() {
-        return "notify_policy_id";
-    }
-
-    @Override
-    protected String getToField() {
-        return "app_system_id";
-    }
-
-    @Override
-    protected List<String> getToFieldList() {
-        return Arrays.asList("app_system_id", "config");
-    }
-
-    @Override
-    protected DependencyInfoVo parse(Object dependencyObj) {
-        if (dependencyObj instanceof Map) {
-            Map<String, Object> map = (Map) dependencyObj;
-            Long appSystemId = (Long) map.get("app_system_id");
-            DeployAppConfigVo appConfigVo = deployAppConfigMapper.getAppConfigVo(new DeployAppConfigVo(appSystemId));
-            if (appConfigVo != null) {
-                IAppSystemMapper iAppSystemMapper = CrossoverServiceFactory.getApi(IAppSystemMapper.class);
-                AppSystemVo appSystemVo = iAppSystemMapper.getAppSystemById(appSystemId);
-                if (appSystemVo != null) {
-                    String lastName = appSystemVo.getAbbrName() + (StringUtils.isNotEmpty(appSystemVo.getName()) ? "(" + appSystemVo.getName() + ")" : "");
-                    JSONObject dependencyInfoConfig = new JSONObject();
-                    dependencyInfoConfig.put("appSystemId", appConfigVo.getAppSystemId());
-                    List<String> pathList = new ArrayList<>();
-                    pathList.add("应用配置");
-                    String urlFormat = "/" + TenantContext.get().getTenantUuid() + "/deploy.html#/application-config-manage?appSystemId=${DATA.appSystemId}";
-                    return new DependencyInfoVo(appConfigVo.getAppSystemId(), dependencyInfoConfig, lastName, pathList, urlFormat, this.getGroupName());
-                }
-            }
-        }
-        return null;
-    }
+//    @Resource
+//    DeployAppConfigMapper deployAppConfigMapper;
+//
+//    @Override
+//    protected String getTableName() {
+//        return "deploy_job_notify_policy";
+//    }
+//
+//    @Override
+//    protected String getFromField() {
+//        return "notify_policy_id";
+//    }
+//
+//    @Override
+//    protected String getToField() {
+//        return "app_system_id";
+//    }
+//
+//    @Override
+//    protected List<String> getToFieldList() {
+//        return Arrays.asList("app_system_id", "config");
+//    }
+//
+//    @Override
+//    protected DependencyInfoVo parse(Object dependencyObj) {
+//        if (dependencyObj instanceof Map) {
+//            Map<String, Object> map = (Map) dependencyObj;
+//            Long appSystemId = (Long) map.get("app_system_id");
+//            DeployAppConfigVo appConfigVo = deployAppConfigMapper.getAppConfigVo(new DeployAppConfigVo(appSystemId));
+//            if (appConfigVo != null) {
+//                IAppSystemMapper iAppSystemMapper = CrossoverServiceFactory.getApi(IAppSystemMapper.class);
+//                AppSystemVo appSystemVo = iAppSystemMapper.getAppSystemById(appSystemId);
+//                if (appSystemVo != null) {
+//                    String lastName = appSystemVo.getAbbrName() + (StringUtils.isNotEmpty(appSystemVo.getName()) ? "(" + appSystemVo.getName() + ")" : "");
+//                    JSONObject dependencyInfoConfig = new JSONObject();
+//                    dependencyInfoConfig.put("appSystemId", appConfigVo.getAppSystemId());
+//                    List<String> pathList = new ArrayList<>();
+//                    pathList.add("应用配置");
+//                    String urlFormat = "/" + TenantContext.get().getTenantUuid() + "/deploy.html#/application-config-manage?appSystemId=${DATA.appSystemId}";
+//                    return new DependencyInfoVo(appConfigVo.getAppSystemId(), dependencyInfoConfig, lastName, pathList, urlFormat, this.getGroupName());
+//                }
+//            }
+//        }
+//        return null;
+//    }
 
     @Override
     public IFromType getFromType() {
         return FrameworkFromType.NOTIFY_POLICY;
+    }
+
+    @Override
+    protected DependencyInfoVo parse(DependencyVo dependencyVo) {
+        Long appSystemId = Long.valueOf(dependencyVo.getTo());
+        IAppSystemMapper iAppSystemMapper = CrossoverServiceFactory.getApi(IAppSystemMapper.class);
+        AppSystemVo appSystemVo = iAppSystemMapper.getAppSystemById(appSystemId);
+        if (appSystemVo != null) {
+            String lastName = appSystemVo.getAbbrName() + (StringUtils.isNotEmpty(appSystemVo.getName()) ? "(" + appSystemVo.getName() + ")" : "");
+            JSONObject dependencyInfoConfig = new JSONObject();
+            dependencyInfoConfig.put("appSystemId", appSystemId);
+            List<String> pathList = new ArrayList<>();
+            pathList.add("应用配置");
+            String urlFormat = "/" + TenantContext.get().getTenantUuid() + "/deploy.html#/application-config-manage?appSystemId=${DATA.appSystemId}";
+            return new DependencyInfoVo(appSystemId, dependencyInfoConfig, lastName, pathList, urlFormat, this.getGroupName());
+        }
+        return null;
     }
 }
