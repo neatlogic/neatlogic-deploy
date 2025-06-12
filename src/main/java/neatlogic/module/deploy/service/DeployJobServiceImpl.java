630d4109cb905b0e5eb1f60334e9aa484f170aca
lvzk <897706680@qq.com>
2025-06-12 20:48:37 +0800

    [功能]自动化作业事务收窄

--- src/main/java/neatlogic/module/deploy/service/DeployJobServiceImpl.java
+++ src/main/java/neatlogic/module/deploy/service/DeployJobServiceImpl.java
@@ -67,7 +67,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.*;
@@ -356,7 +355,6 @@ public class DeployJobServiceImpl implements DeployJobService {
         return createJob(deployJobParam, new DeployJobModuleVo());
     }
 
-    @Transactional
     @Override
     public JSONObject createJobAndFire(DeployJobVo deployJobParam, DeployJobModuleVo module) throws Exception {
         JSONObject resultJson = createJob(deployJobParam, module);
