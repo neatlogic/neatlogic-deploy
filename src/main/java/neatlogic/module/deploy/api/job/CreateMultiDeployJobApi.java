76fd5f8c85624fb2dbcbacb13dd8d93d7c4b5e2c
lvzk <897706680@qq.com>
2025-07-08 20:19:39 +0800

    [功能]自动化支持选择并发数 #[1447956513783808]自动化支持选择并发数 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1447956513783808

--- src/main/java/neatlogic/module/deploy/api/job/CreateMultiDeployJobApi.java
+++ src/main/java/neatlogic/module/deploy/api/job/CreateMultiDeployJobApi.java
@@ -86,7 +86,7 @@ public class CreateMultiDeployJobApi extends PrivateApiComponentBase {
             @Param(name = "envId", type = ApiParamType.LONG, desc = "term.cmdb.envid"),
             @Param(name = "envName", type = ApiParamType.STRING, desc = "term.cmdb.envname", help = "如果入参也有envId，则会以envName为准"),
             @Param(name = "param", type = ApiParamType.JSONOBJECT, desc = "term.autoexec.executeparam"),
-            @Param(name = "roundCount", type = ApiParamType.LONG, isRequired = true, desc = "term.autoexec.roundcount"),
+            @Param(name = "roundCount", type = ApiParamType.LONG, desc = "term.autoexec.roundcount"),
             @Param(name = "executeConfig", type = ApiParamType.JSONOBJECT, desc = "term.autoexec.executeconfig"),
             @Param(name = "planStartTime", type = ApiParamType.LONG, desc = "common.planstarttime"),
             @Param(name = "triggerType", type = ApiParamType.ENUM, rule = "auto,manual", desc = "common.triggertype"),
