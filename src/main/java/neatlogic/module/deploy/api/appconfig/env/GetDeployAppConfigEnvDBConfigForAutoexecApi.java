1c822baeaac68b89277a6a5cfdfb292959386cf7
1437892690@qq.com <1437892690@qq.com>
2025-12-02 17:47:55 +0800

    [修复] 资产清单根模型设置为操作系统时，创建作业时报错缺少可执行节点
    关联 #[1559494255935488]资产清单根模型设置为操作系统时，创建作业时报错缺少可执行节点 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1559494255935488

--- src/main/java/neatlogic/module/deploy/api/appconfig/env/GetDeployAppConfigEnvDBConfigForAutoexecApi.java
+++ src/main/java/neatlogic/module/deploy/api/appconfig/env/GetDeployAppConfigEnvDBConfigForAutoexecApi.java
@@ -4,11 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.cmdb.crossover.IAttrCrossoverMapper;
 import neatlogic.framework.cmdb.crossover.ICiEntityCrossoverService;
-import neatlogic.framework.cmdb.crossover.IResourceEntityCrossoverMapper;
+import neatlogic.framework.cmdb.crossover.IResourceEntityCrossoverService;
 import neatlogic.framework.cmdb.dto.ci.AttrVo;
+import neatlogic.framework.cmdb.dto.ci.CiVo;
 import neatlogic.framework.cmdb.dto.cientity.AttrEntityVo;
 import neatlogic.framework.cmdb.dto.cientity.CiEntityVo;
-import neatlogic.framework.cmdb.dto.resourcecenter.config.ResourceEntityVo;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.crossover.CrossoverServiceFactory;
 import neatlogic.framework.deploy.auth.DEPLOY_BASE;
@@ -80,10 +80,10 @@ public class GetDeployAppConfigEnvDBConfigForAutoexecApi extends PrivateApiCompo
         Set<Long> dbResourceIdSet = allDBConfigVoList.stream().map(DeployAppConfigEnvDBConfigVo::getDbResourceId).collect(Collectors.toSet());
         //获取db属性
         List<CiEntityVo> allDBResourceInfoList = null;
-        IResourceEntityCrossoverMapper resourceEntityCrossoverMapper = CrossoverServiceFactory.getApi(IResourceEntityCrossoverMapper.class);
-        ResourceEntityVo resourceEntityVo = resourceEntityCrossoverMapper.getResourceEntityByName("scence_database_ip_port_env_appmodule");
-        if (resourceEntityVo != null) {
-            Long ciId = resourceEntityVo.getCiId();
+        IResourceEntityCrossoverService resourceEntityCrossoverService = CrossoverServiceFactory.getApi(IResourceEntityCrossoverService.class);
+        CiVo rootCiVo = resourceEntityCrossoverService.getViewRootCi("scence_database_ip_port_env_appmodule");
+        if (rootCiVo != null) {
+            Long ciId = rootCiVo.getId();
             if (ciId != null) {
                 CiEntityVo paramCiEntityVo = new CiEntityVo();
                 List<Long> attrIdList = new ArrayList<>();
