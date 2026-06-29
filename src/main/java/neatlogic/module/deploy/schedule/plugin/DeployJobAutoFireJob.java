095dd716cf34a90d5ad830a60d64e8c4f82ae9d5
1437892690@qq.com <1437892690@qq.com>
2026-06-29 18:46:09 +0800

    [功能] serverid增加分组概念：接管作业只能接管同组server；定时作业只能看到自己组内的数据
    关联 #[1416699956396032]serverid增加分组概念：接管作业只能接管同组server；定时作业只能看到自己组内的数据 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1416699956396032

--- src/main/java/neatlogic/module/deploy/schedule/plugin/DeployJobAutoFireJob.java
+++ src/main/java/neatlogic/module/deploy/schedule/plugin/DeployJobAutoFireJob.java
@@ -30,6 +30,7 @@ import neatlogic.framework.dto.UserVo;
 import neatlogic.framework.filter.core.LoginAuthHandlerBase;
 import neatlogic.framework.scheduler.core.JobBase;
 import neatlogic.framework.scheduler.dto.JobObject;
+import neatlogic.framework.scheduler.enums.JobLoadTriggerType;
 import neatlogic.framework.service.AuthenticationInfoService;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.quartz.DisallowConcurrentExecution;
@@ -80,7 +81,7 @@ public class DeployJobAutoFireJob extends JobBase {
     }
 
     @Override
-    public void reloadJob(JobObject jobObject) {
+    public void reloadJob(JobObject jobObject, JobLoadTriggerType triggerType) {
         String tenantUuid = jobObject.getTenantUuid();
         TenantContext.get().switchTenant(tenantUuid);
         Long jobId = Long.valueOf(jobObject.getJobName());
@@ -94,7 +95,7 @@ public class DeployJobAutoFireJob extends JobBase {
                             .withIntervalInSeconds(60 * 60)
                             .withRepeatCount(0);
                     JobObject newJobObject = newJobObjectBuilder.build();
-                    schedulerManager.loadJob(newJobObject);
+                    schedulerManager.loadJob(newJobObject, triggerType);
                 } else {
                     fireJob(jobVo);
                 }
@@ -110,7 +111,7 @@ public class DeployJobAutoFireJob extends JobBase {
         for (Long id : list) {
             JobObject.Builder jobObjectBuilder = new JobObject.Builder(id.toString(), this.getGroupName(), this.getClassName(), TenantContext.get().getTenantUuid());
             JobObject jobObject = jobObjectBuilder.build();
-            this.reloadJob(jobObject);
+            this.reloadJob(jobObject, JobLoadTriggerType.SERVER_RESTART);
         }
     }
 
